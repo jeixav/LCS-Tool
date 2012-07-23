7b699b684750b0eca055d45e7f6bd5b831dbca63
Kristjan Onu <kristjan.onu@mcgill.ca>
2012-07-23 14:54:36 -0400

    Rename variable to follow convention

--- impose_periodic_bc.m
+++ impose_periodic_bc.m
@@ -1,7 +1,7 @@
-function position = impose_periodic_bc(position,domain,periodic_bc_spec)
+function position = impose_periodic_bc(position,domain,periodicBcSpec)
 
-for i = 1:size(periodic_bc_spec,2)
-    if periodic_bc_spec(i)
+for i = 1:size(periodicBcSpec,2)
+    if periodicBcSpec(i)
         length = domain(i,2) - domain(i,1);
         position(:,i,:) = mod(position(:,i,:) + .5*length,length) - .5*length;
     end
