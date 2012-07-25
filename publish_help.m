21e1b6c02f160822c495ef70a04f67729d81aa89
Kristjan Onu <kristjan.onu@mcgill.ca>
2012-07-25 11:26:51 -0400

    Create function to create documentation

---
+++ publish_help.m
@@ -0,0 +1,9 @@
+function publish_help
+%PUBLISH_HELP Create LCS Toolbox help browser documentation
+
+publish('strain_lcs_script_help.m')
+publish('lcs_functions.m')
+publish('lcs_product_page.m')
+
+end
+
