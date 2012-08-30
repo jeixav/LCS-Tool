66f09c80d1abf5fa70a50dc688cec8225abd6663
Samer Barakat <sbarakat@purdue.edu>
2012-08-29 22:50:04 -0400

    update to closed cycle detection

--- compute_shear_lcs_closed.m
+++ compute_shear_lcs_closed.m
@@ -1,27 +1,39 @@
 function [flow,shearline] = compute_shear_lcs_closed(flow,shearline)
 
 if ~all(isfield(flow,{'cgEigenvalue','cgEigenvector'}))
-    verbose.progress = true;
+    verbose.progressBar = true;
     verbose.stats = false;
     eigenvalueFromMainGrid = true;
-    [flow.cgEigenvalue,flow.cgEigenvector] = eig_cgStrain(flow,...
-        eigenvalueFromMainGrid,verbose);
+   % [flow.cgEigenvalue,flow.cgEigenvector] = eig_cgStrain(flow,...
+    %    eigenvalueFromMainGrid,verbose);
+	
+	flow.cgEigenvalue = zeros(flow.resolution(1) * flow.resolution(2), 2);
+	flow.cgEigenvector = zeros(flow.resolution(1) * flow.resolution(2), 4);
 end
 
 %if ~all(isfield(shearline,{'etaPos','etaNeg','positionPos','positionNeg'}))
     verbose = true;
     %shearline = compute_shearline(flow,shearline,verbose);
 	
+	% convert tensor to array
+	cgStrainTensor = zeros(flow.resolution(1) * flow.resolution(2), 4);
+	%for n=1:length(flow.cgStrain)
+	%	cgStrainTensor(n,1)=flow.cgStrain{n}(1,1);
+	%	cgStrainTensor(n,2)=flow.cgStrain{n}(1,2);
+	%	cgStrainTensor(n,3)=flow.cgStrain{n}(2,1);
+	%	cgStrainTensor(n,4)=flow.cgStrain{n}(2,2);
+	%end
+	
+	% call my function
 	addpath('ClosedIntegralLines');
 	flow.timespan
 	flow.domain
 	flow.resolution
 	shearline.positionPos = []
 	shearline.positionNeg = []
-	[shearline.positionPos,shearline.positionNeg]=compute_closed_shearline(flow.timespan, flow.domain, flow.resolution, flow.cgEigenvalue, flow.cgEigenvector);
-	%shearline.positionNeg=shearline.positionPos
-	shearline.positionPos=shearline.positionPos'
-	shearline.positionNeg=shearline.positionNeg'
+	[shearline.positionPos,shearline.positionNeg]=compute_closed_shearline(flow.timespan, flow.domain, flow.resolution, flow.cgEigenvalue, flow.cgEigenvector, cgStrainTensor);
+	shearline.positionPos=shearline.positionPos';
+	shearline.positionNeg=shearline.positionNeg';
 	
 	shearline.geodesicDeviationPos=shearline.positionPos;
 	shearline.geodesicDeviationNeg=shearline.positionNeg;
