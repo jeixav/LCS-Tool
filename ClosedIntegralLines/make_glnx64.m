52c1a2cc623cd5e1c758f389372a687b2a4b1211
Kristjan Onu <kristjan.onu@mcgill.ca>
2012-09-05 10:35:35 -0400

    Open MPI modification

--- ClosedIntegralLines/make_glnx64.m
+++ ClosedIntegralLines/make_glnx64.m
@@ -1,4 +1,4 @@
 userHome = getenv('HOME');
 
-mex('-v','-largeArrayDims','CC=gcc-4.4','compute_closed_shearline.cpp',...
-    'RegularGrid.cpp','-lgsl','-lgslcblas')
+mex('-v','-largeArrayDims','CC=gcc-4.4','CXXFLAGS=\$CFLAGS -fopenmp',...
+    'compute_closed_shearline.cpp','RegularGrid.cpp','-lgsl','-lgslcblas')
