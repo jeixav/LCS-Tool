f58805227d6fc85786d6132f551dfc0008cced16
Kristjan Onu <kristjan.onu@mcgill.ca>
2012-09-10 08:39:56 -0400

    Code simplification

--- initialize_ic_grid.m
+++ initialize_ic_grid.m
@@ -1,10 +1,18 @@
+%initialize_ic_grid Initialize initial conditions on cartesian grid
 function position = initialize_ic_grid(resolution,domain)
-%initialize_ic_grid Initialize initial conditions on cartesian grid.
 
-deltaX = (domain(1,2) - domain(1,1))/double(resolution(1));
-deltaY = (domain(2,2) - domain(2,1))/double(resolution(2));
-[positionX positionY] = meshgrid(...
-    domain(1,1) + .5*deltaX:deltaX:domain(1,2) - .5*deltaX,...
-    domain(2,1) + .5*deltaY:deltaY:domain(2,2) - .5*deltaY);
+xVector = shift_ic(resolution(1),domain(1,:));
+yVector = shift_ic(resolution(2),domain(2,:));
+
+[positionX positionY] = meshgrid(xVector,yVector);
 position(:,1) = reshape(positionX,numel(positionX),1);
 position(:,2) = reshape(positionY,numel(positionY),1);
+
+% shift_ic Place all positions inside domain
+function vector = shift_ic(resolution,domain)
+
+delta = (domain(2) - domain(1))/double(resolution);
+
+vector = linspace(domain(1),domain(2),resolution+1);
+vector = vector + .5*delta;
+vector = vector(1:end-1);
