7b313da25fe5024efd9ea84e5c72042883f8767d
Kristjan Onu <Kristjan Onu jeixav@gmail.com>
2012-10-23 14:48:46 -0400

    Enable parallel processing with parfor

--- integrate_flow2.m
+++ integrate_flow2.m
@@ -1,13 +1,50 @@
-function flowSolution = integrate_flow2(flow,initialPosition)
-
-initialPosition = to_coupled(initialPosition);
+function flowSolution = integrate_flow2(flow,initialPosition,verbose)
 
 if ~isfield(flow,'odeSolver')
     flow.odeSolver = @ode45;
 end
 
-flowSolution = ode_ic_array(flow.odeSolver,flow.derivative,flow.timespan,...
-    initialPosition);
+if nargin < 3
+    verbose = true;
+end
+
+method = 'parfor';
+
+switch method
+    case 'arrayfun'
+        initialPosition = to_coupled(initialPosition);
+        
+        flowSolution = ode_ic_array(flow.odeSolver,flow.derivative,flow.timespan,...
+            initialPosition);
+    case 'parfor'
+
+        nPosition = prod(double(flow.resolution));
+
+        if verbose
+            progressBar = ParforProgressStarter2(mfilename,nPosition);
+        end
+        
+        odeSolver = flow.odeSolver;
+        odefun = flow.derivative;
+        timespan = flow.timespan;
+        parfor iPosition = 1:nPosition
+            flowSolution(iPosition) = feval(odeSolver,odefun,...
+                timespan,initialPosition(iPosition,:));
+            if verbose
+                progressBar.increment(iPosition) %#ok<PFBNS>
+            end
+        end
+        
+        if verbose
+            try
+                delete(progressBar);
+            catch me %#ok<NASGU>
+            end
+        end
+
+    otherwise
+        error('Invalid method selected')
+end
 
 function sol = ode_ic_array(odeSolver,odefun,tspan,y0Array,options)
 
