b76d258d71e1a887ff7f3198e07a82c75f677ee6
Kristjan Onu <Kristjan Onu jeixav@gmail.com>
2012-10-23 14:51:27 -0400

    Allow specifying only the x-resolution

--- set_flow_resolution.m
+++ set_flow_resolution.m
@@ -2,9 +2,23 @@ function flow = set_flow_resolution(resolution,flow)
 % Set the flow resolution and delete fields that depend on it from the
 % flow, strainline and shearline structures.
 %
-% Example: flow = set_flow_resolution(uint64([400 200]),flow)
+% The resolution can be specified as a 1x2 matrix or a scalar. If it is a
+% scalar, the number given is used for the x-resolution and the
+% y-resolution is set automatically to have a square grid.
+%
+% Example: flow = set_flow_resolution([400 200],flow)
 
-validateattributes(resolution,{'uint64'},{'size',[1 2],'positive'})
+validateattributes(resolution,{'numeric'},{'row','positive'})
+
+if isscalar(resolution)
+    resX = resolution;
+    resY = resX/diff(flow.domain(1,:))*diff(flow.domain(2,:));
+    resolution = [resX resY];
+end
+    
+if ~isa(resolution,'uint64')
+    resolution = uint64(resolution);
+end
 
 flow.resolution = resolution;
 
