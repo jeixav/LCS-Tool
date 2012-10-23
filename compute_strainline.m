153a597a8c31a54e7ffd9b6ede7c1a9c11b4aba8
Kristjan Onu <Kristjan Onu jeixav@gmail.com>
2012-10-23 14:43:23 -0400

    Switch parallel processing from SPMD to parfor

--- compute_strainline.m
+++ compute_strainline.m
@@ -1,5 +1,7 @@
 function strainline = compute_strainline(flow,strainline,verbose)
 
+narginchk(2,3)
+
 if nargin < 3
     verbose.progress = false;
 end
@@ -14,61 +16,51 @@ if isfield(strainline,'resolution')
 end
 
 nStrainlines = size(strainline.initialPosition,1);
-iStrainline = 1:nStrainlines;
 
 if ~isfield(strainline,'odeSolverOptions')
     strainline.odeSolverOptions = [];
 end
 
-spmdFun = @(idx)integrate_line(timespan,...
-    strainline.initialPosition(idx,:),flow.domain,flow.resolution,...
-    flow.cgEigenvector(:,1:2),strainline.odeSolverOptions);
 
 if verbose.progress
-    progressBar = ConsoleProgressBar;
-    progressBar.setText(mfilename)
-    progressBar.setTextPosition('left')
-    progressBar.setElapsedTimeVisible(1)
-    progressBar.setRemainedTimeVisible(1)
-    progressBar.setLength(20)
-    progressBar.setMaximum(2*nStrainlines)
-    progressBar.start
-end
-
-spmd
-    iStrainlineC = codistributed(iStrainline);
-    iStrainlineL = getLocalPart(iStrainlineC);
-    positionL = arrayfun(spmdFun,iStrainlineL,'UniformOutput',false);
-    positionC = codistributed.build(positionL,...
-        getCodistributor(iStrainlineC));
-end
-
-strainline.position = gather(positionC);
-progressBar.setValue(nStrainlines)
-
-% Backward time integration
-timespan = -timespan;
-
-spmdFun = @(idx)integrate_line(timespan,...
-    strainline.initialPosition(idx,:),flow.domain,flow.resolution,...
-    flow.cgEigenvector(:,1:2),strainline.odeSolverOptions);
-
-spmd
-    iStrainlineC = codistributed(iStrainline);
-    iStrainlineL = getLocalPart(iStrainlineC);
-    positionL = arrayfun(spmdFun,iStrainlineL,'UniformOutput',false);
-    positionC = codistributed.build(positionL,...
-        getCodistributor(iStrainlineC));
-end
-
-if verbose.progress
-    progressBar.setValue(progressBar.maximum)
-    progressBar.stop
-    fprintf('\n')
+    progressBar = ParforProgressStarter2(mfilename,2*nStrainlines);
 end
         
+parforFun = @(idx)integrate_line(timespan,...
+    strainline.initialPosition(idx,:),flow.domain,flow.resolution,...
+    flow.cgEigenvector(:,1:2),strainline.odeSolverOptions);
+
+parfor i = 1:nStrainlines
+    positionForward{i} = feval(parforFun,i);
+    if verbose.progress %#ok<PFBNS>
+        progressBar.increment(i) %#ok<PFBNS>
+    end
+end
+        
+strainline.position = positionForward;
+        
+timespan = -timespan;
+parforFun = @(idx)integrate_line(timespan,...
+    strainline.initialPosition(idx,:),flow.domain,flow.resolution,...
+    flow.cgEigenvector(:,1:2),strainline.odeSolverOptions);
+
+parfor i = 1:nStrainlines
+    positionBackward{i} = feval(parforFun,i);
+    if verbose.progress %#ok<PFBNS>
+        progressBar.increment(i+nStrainlines) %#ok<PFBNS>
+    end
+end
+        
+if verbose.progress
+    try
+        delete(progressBar);
+    catch me %#ok<NASGU>
+    end
+end
+
 % Concatenate forward and backward time integration results
-tmp = cellfun(@(input)flipud(input(2:end,:)),gather(positionC),...
+tmp = cellfun(@(input)flipud(input(2:end,:)),positionBackward,...
     'uniformOutput',false);
 strainline.position = cellfun(@(a,b)[a;b],tmp,strainline.position,...
     'UniformOutput',false);
+     
