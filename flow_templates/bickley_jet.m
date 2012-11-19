38a6a7cecae3ec5c0a0c64e4cd8ee663d559fd4d
Kristjan Onu <kristjan.onu@mcgill.ca>
2012-11-19 13:10:40 -0500

    Non-dimensionalize Bickley jet

--- flow_templates/bickley_jet.m
+++ flow_templates/bickley_jet.m
@@ -1,23 +1,42 @@
-function bickleyJet = bickley_jet(perturbationCase)
 % Describes a bickley jet.
 %
 % References: doi:10.1016/j.physd.2012.06.012, doi:10.1063/1.3271342
 
-p.u = 62.66;
-p.c2 = .205*p.u;
-p.c3 = .461*p.u;
-a = 6371e3;
-p.lengthX = pi*a;
-p.lengthY = 1770e3;
-k = @(n) 2*n*pi/p.lengthX;
+function bickleyJet = bickley_jet_non_dim(perturbationCase,p)
 
-p.epsilon1 = .075;
-p.epsilon2 = .4;
-% p.epsilon3 = .3;
-p.epsilon3 = 0;
+narginchk(1,2)
+
+defaultP.u = 62.66;
+defaultP.c2 = .205*defaultP.u;
+defaultP.c3 = .461*defaultP.u;
+defaultP.a = 6.371e6;
+defaultP.lengthX = pi*defaultP.a;
+defaultP.lengthY = 1.77e6;
+
+defaultP.epsilon1 = .075;
+defaultP.epsilon2 = .4;
+defaultP.epsilon3 = .3;
+
+if nargin < 2
+    p = defaultP;
+else
+    fieldnamesDefaultP = fieldnames(defaultP);
+    for i = 1:numel(fieldnamesDefaultP)
+        if ~isfield(p,fieldnamesDefaultP{i}) || ...
+                isempty(p.(fieldnamesDefaultP{i}))
+            p.(fieldnamesDefaultP{i}) = defaultP.(fieldnamesDefaultP{i});
+        end
+    end
+end
+
+k = @(n) 2*n*pi/p.lengthX;
 
 sigma1 = .5*k(2)*(p.c2 - p.c3);
 sigma2 = 2*sigma1;
+
+timescale = p.lengthX/p.u;
+lengthscaleX = p.lengthX/2/pi;
+lengthscaleY = p.lengthY;
 
 switch perturbationCase
     case {1,3}
@@ -30,31 +49,10 @@ switch perturbationCase
         % Time-periodic psi1, case 1 on page 1691 of
         % doi:10.1016/j.physd.2012.06.012.
  
-        % t = sym('t');
-        % x = sym('x');
-        % y = sym('y');
-
         f1 = @(t)exp(1i*sigma1*t);
         f2 = @(t)exp(1i*sigma2*t);
 
-        % psi(1) = p.c3*y - p.u*p.lengthY*tanh(y/p.lengthY) ...
-        %     + p.epsilon3*p.u*p.lengthY*(sech(y/p.lengthY))^2*cos(k(3)*x);
-
-        % psi(2) = p.u*p.lengthY*(sech(y/p.lengthY))^2 ...
-        %     *real(p.epsilon1*f1(t)*exp(1i*k(1)*x) ...
-        %     + p.epsilon2*f2(t)*exp(1i*k(2)*x));
-
-        % bickleyJet.flow.symDerivative(1) = -diff(psi(1) + psi(2),y);
-        % bickleyJet.flow.symDerivative(2) = diff(psi(1) + psi(2),x);
-
         u = @(t,x)-p.c3 - p.u*(tanh(x(2)/p.lengthY)^2 - 1) ...
-            + 2*p.u*sech(x(2)/p.lengthY)^2 ...
-            *tanh(x(2)/p.lengthY) ...
-            *(real(p.epsilon1*f1(t)*exp(k(1)*x(1)*1i) ...
-            + p.epsilon2*f2(t)*exp(k(2)*x(1)*1i)) ...
-            + p.epsilon3*cos(k(3)*x(1)));
-        % FIXME Terms set to 0 to match Figure 16
-        u = @(t,x)-0*p.c3 - 0*p.u*(tanh(x(2)/p.lengthY)^2 - 1) ...
             + 2*p.u*sech(x(2)/p.lengthY)^2 ...
             *tanh(x(2)/p.lengthY) ...
             *(real(p.epsilon1*f1(t)*exp(k(1)*x(1)*1i) ...
@@ -65,7 +63,10 @@ switch perturbationCase
             + p.epsilon2*f2(t)*k(2)*exp(k(2)*x(1)*1i)) ...
             + p.epsilon3*k(3)*sin(k(3)*x(1)));
         
-        bickleyJet.flow.derivative = @(t,x)[u(t,x); v(t,x)];
+        lengthscale = [lengthscaleX; lengthscaleY];
+        bickleyJet.flow.derivative = @(t,x)...
+            [2*u(timescale*t,lengthscale.*x)/lengthscaleX; ...
+            v(timescale*t,lengthscale.*x)/lengthscaleY]*timescale;
 
         dux = @(t,x)-2*p.u ...
             *sech(x(2)/p.lengthY)^2*tanh(x(2)/p.lengthY) ...
@@ -86,8 +87,11 @@ switch perturbationCase
             + imag(p.epsilon2*f2(t)*k(2)*exp(k(2)*x(1)*1i)) ...
             + p.epsilon3*k(3)*sin(k(3)*x(1)));
         
-        bickleyJet.flow.dDerivative = @(t,x)[dux(t,x) duy(t,x); ...
-            dvx(t,x) dvy(t,x)];
+        bickleyJet.flow.dDerivative = ...
+            @(t,x)[2*dux(timescale*t,lengthscale.*x)/lengthscaleX*lengthscaleX ...
+            2*duy(timescale*t,lengthscale.*x)/lengthscaleX*lengthscaleY; ...
+            dvx(timescale*t,lengthscale.*x)/lengthscaleY*lengthscaleX ...
+            dvy(timescale*t,lengthscale.*x)/lengthscaleY*lengthscaleY]*timescale;
     
     case {2,3}
         % Time-aperiodic psi1, case 2 on page 1691 of
@@ -112,9 +116,6 @@ switch perturbationCase
         phi2Int = griddedInterpolant(linspace(phiTimespan(1),...
             phiTimespan(2),timeResolution),phi2);
         
-        sigma1 = .5*k(2)*(p.c2 - p.c3);
-        sigma2 = 2*sigma1;
-        
         beronVeraT = max(2*pi./abs([sigma1 sigma2]));
         
         % Find maximum value of phi
@@ -122,7 +123,8 @@ switch perturbationCase
         % maxSamples = 1e3;
         phi2Max = max(phi2);
         
-        beronVeraMagicScaleAmp = 1.75;
+        amplitudeCorrection = .015;
+        beronVeraMagicScaleAmp = 1.75*amplitudeCorrection;
         beronVeraMagicScaleTime = beronVeraT/beronVeraW;
         f1 = @(t)beronVeraMagicScaleAmp*phi2Int(t ...
             /beronVeraMagicScaleTime)*phi2Max;
@@ -139,8 +141,12 @@ switch perturbationCase
             + imag(p.epsilon2*f2(t)*k(2)*exp(k(2)*x(1)*1i)) ...
             + p.epsilon3*k(3)*sin(k(3)*x(1)));
         
-        bickleyJet.flow.derivative = @(t,x)[u(t,x); v(t,x)];
+        bickleyJet.flow.derivative = @(t,x)...
+            [u(t*timescale,[x(1)*lengthscaleX x(2)*lengthscaleY]); ...
+            v(t*timescale,[x(1)*lengthscaleX x(2)*lengthscaleY])];
 
+        % FIXME These need not be defined unless using the equation of
+        % variation method
         dux = @(t,x)-2*p.u ...
             *sech(x(2)/p.lengthY)^2*tanh(x(2)/p.lengthY) ...
             *(imag(p.epsilon1*f1(t)*k(1)*exp(k(1)*x(1)*1i)) ...
@@ -170,21 +176,21 @@ end
 bickleyJet.flow.imposeIncompressibility = true;
 bickleyJet.flow.periodicBc = [true false];
 
-% FIXME In doi:10.1016/j.physd.2012.06.012, x_max = 2*pi*p.lengthX.
-bickleyJet.flow = set_flow_domain([0 .3*pi*p.lengthX; ...
-    [-1 1]*2.25*p.lengthY],bickleyJet.flow);
-bickleyJet.flow = set_flow_timespan([0 4*p.lengthX/p.u],bickleyJet.flow);
+bickleyJet.flow = set_flow_domain([0 2*pi; [-1 1]*2],bickleyJet.flow);
 
-bickleyJet.flow = set_flow_resolution(20,bickleyJet.flow);
+finalTime = 4;
+bickleyJet.flow = set_flow_timespan([0 finalTime],bickleyJet.flow);
 
-bickleyJet.strainline = set_strainline_resolution(uint64([4 3]));
-bickleyJet.strainline = set_strainline_max_length(1e6,bickleyJet.strainline);
+bickleyJet.flow = set_flow_resolution(100,bickleyJet.flow);
+
+bickleyJet.strainline = set_strainline_resolution(uint64([2 1]*10));
+bickleyJet.strainline = set_strainline_max_length(20,bickleyJet.strainline);
 bickleyJet.strainline = set_strainline_geodesic_deviation_tol(inf,...
     bickleyJet.strainline);
 bickleyJet.strainline = set_strainline_length_tol(0,bickleyJet.strainline);
-bickleyJet.strainline.filteringMethod = 'superminimization';
+bickleyJet.strainline.filteringMethod = 'superminimize';
 bickleyJet.strainline.filteringParameters = struct('distance',0,...
-    'resolution',uint64([1 1]));
+    'resolution',uint64([5 2]));
 
 bickleyJet.shearline = set_shearline_resolution(uint64([2 1]*5));
 bickleyJet.shearline = set_shearline_max_length(.1,bickleyJet.shearline);
