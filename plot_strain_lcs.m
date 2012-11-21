b609f6fbf5e592aa16cf6db9ba2f2bdf16ed92df
Kristjan Onu <kristjan.onu@mcgill.ca>
2012-11-21 10:36:18 -0500

    Remove arrowheads in strainline quiver plot

--- plot_strain_lcs.m
+++ plot_strain_lcs.m
@@ -2,10 +2,13 @@ function plot_strain_lcs(axes,flow,strainline,showPlot)
 
 if isfield(flow,'cgEigenvector')
     cgPosition = initialize_ic_grid(flow.resolution,flow.domain);
-    quiver(axes,cgPosition(:,1),cgPosition(:,2),...
-        flow.cgEigenvector(:,1),flow.cgEigenvector(:,2),.5,'tag','quiver')
+    hQuiver = quiver(axes,cgPosition(:,1),cgPosition(:,2),...
+        flow.cgEigenvector(:,1),flow.cgEigenvector(:,2));
+    set(hQuiver,'ShowArrowHead','off')
+    set(hQuiver,'AutoScaleFactor',.5)
+    set(hQuiver,'tag','quiver')
     if ~isfield(showPlot,'quiver') || showPlot.quiver == false;
-        set(findobj(axes,'tag','quiver'),'visible','off')
+        set(hQuiver,'visible','off')
     end
 end
 
