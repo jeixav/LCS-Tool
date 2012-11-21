71d391f306a382421103157951e238c1c7913920
Kristjan Onu <kristjan.onu@mcgill.ca>
2012-11-21 10:56:36 -0500

    Change double gyre flow template
    Parameters changed to provide physically reasonable results at the
    expense of increasing computation time.

--- flow_templates/double_gyre.m
+++ flow_templates/double_gyre.m
@@ -1,8 +1,21 @@
-function doubleGyre = double_gyre
-% Describes a double gyre flow.
+% double_gyre Defines double gyre flow for LCS analysis
 %
-% References: doi:10.1016/j.physd.2005.10.007, doi:10.5194/npg-7-59-2000,
+% REFERENCES
+% doi:10.1016/j.physd.2005.10.007
+% doi:10.5194/npg-7-59-2000,
 % doi:10.5194/npg-4-223-1997
+%
+% EXAMPLE
+% matlabpool('open')
+% pctRunOnAll javaaddpath('ParforProgress2')
+% addpath('flow_templates')
+% doubleGyre = double_gyre;
+% doubleGyre = strain_lcs_script(doubleGyre);
+% set(findobj(gca,'tag','strainline'),'visible','on')
+
+% Copyright 2012 Kristjan Onu
+
+function doubleGyre = double_gyre
 
 t = sym('t');
 x = sym('x');
@@ -19,11 +32,11 @@ doubleGyre.flow.symDerivative(2) = pi*p.a*cos(pi*forcing).*sin(pi*y)...
 
 doubleGyre.flow = set_flow_domain([0 2; 0 1],doubleGyre.flow);
 doubleGyre.flow = set_flow_timespan([0 20],doubleGyre.flow);
-doubleGyre.flow = set_flow_resolution([2 1]*10,doubleGyre.flow);
+doubleGyre.flow = set_flow_resolution([2 1]*100,doubleGyre.flow);
 
 doubleGyre.flow.imposeIncompressibility = true;
 
-doubleGyre.strainline = set_strainline_resolution([2 1]*5);
+doubleGyre.strainline = set_strainline_resolution([2 1]*10);
 doubleGyre.strainline = set_strainline_max_length(10,doubleGyre.strainline);
 doubleGyre.strainline = set_strainline_geodesic_deviation_tol(inf,...
     doubleGyre.strainline);
@@ -31,7 +44,9 @@ doubleGyre.strainline = set_strainline_length_tol(0,doubleGyre.strainline);
 doubleGyre.strainline = set_strainline_filtering_method('superminimize',...
     doubleGyre.strainline);
 doubleGyre.strainline = set_strainline_filtering_parameters(...
-    struct('distance',0,'resolution',[1 1]),doubleGyre.strainline);
+    struct('distance',1.5,'resolution',[1 1]),doubleGyre.strainline);
+doubleGyre.strainline = set_strainline_ode_solver_options(...
+    odeset('relTol',1e-6),doubleGyre.strainline);
 
 doubleGyre.shearline = set_shearline_resolution(uint64([2 1]*5));
 doubleGyre.shearline = set_shearline_max_length(10,doubleGyre.shearline);
