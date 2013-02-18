14aca50f3d043e322bf3dcc75c24528a3e695c41
Kristjan Onu <kristjan.onu@mcgill.ca>
2013-02-18 16:01:11 -0500

    Miscellaneous changes

--- plot_orient_discont.m
+++ plot_orient_discont.m
@@ -1,15 +1,17 @@
 % plot_orient_discont Plot eigenvector field orientation discontinuities
 %
+% SYNTAX
+% hAxes = plot_orient_discont(eigenvector,domain,resolution)
+% hAxes = plot_orient_discont(eigenvector,domain,resolution,angleTol)
+%
 % EXAMPLE
 %   s = load('datasets/bickley_jet/bickleyJet1.mat');
-%   eigenvector = [s.bickleyJet.flow.cgEigenvector(:,1),...
-%       s.bickleyJet.flow.cgEigenvector(:,2)];
-%   a1 = plot_orient_discont(eigenvector,s.bickleyJet.flow.domain,...
-%       s.bickleyJet.flow.resolution);
+%   eigenvector = [s.bickleyJet.flow.cgEigenvector(:,1),s.bickleyJet.flow.cgEigenvector(:,2)];
+%   a1 = plot_orient_discont(eigenvector,s.bickleyJet.flow.domain,s.bickleyJet.flow.resolution);
 %   zoomDomain = [2.6,2.7;-.04,.01];
 %   set(a1,'xlim',zoomDomain(1,:),'ylim',zoomDomain(2,:))
 
-function a1 = plot_orient_discont(eigenvector,domain,resolution,angleTol)
+function hAxes = plot_orient_discont(eigenvector,domain,resolution,angleTol)
 
 narginchk(3,4)
 
@@ -18,15 +20,14 @@ if nargin == 3
 end
 
 f1 = figure;
-a1 = axes('parent',f1,...
+hAxes = axes('parent',f1,...
     'box','on',...
     'NextPlot','add',...
     'DataAspectRatio',[1 1 1],...
     'DataAspectRatioMode','manual');
 
 position = initialize_ic_grid(resolution,domain);
-hQuiver = quiver(a1,position(:,1),position(:,2),...
-    eigenvector(:,1),eigenvector(:,2));
+hQuiver = quiver(hAxes,position(:,1),position(:,2),eigenvector(:,1),eigenvector(:,2));
 set(hQuiver,'AutoScaleFactor',.5)
 
 positionGridX = reshape(position(:,1),fliplr(resolution));
@@ -34,18 +35,14 @@ positionGridY = reshape(position(:,2),fliplr(resolution));
 
 eigenvector = reshape(eigenvector,[fliplr(resolution) 2]);
 
-idx1Discont = idx1_discont(eigenvector(:,:,1),eigenvector(:,:,2),...
-    resolution,angleTol);
+idx1Discont = idx1_discont(eigenvector(:,:,1),eigenvector(:,:,2),resolution,angleTol);
 deltaX = .5*diff(domain(1,:))/(double(resolution(1)) - 1);
-hDiscont = plot(a1,positionGridX(idx1Discont)+deltaX,...
-    positionGridY(idx1Discont),'ro');
+hDiscont = plot(hAxes,positionGridX(idx1Discont)+deltaX,positionGridY(idx1Discont),'ro');
 set(hDiscont,'MarkerFaceColor','r')
 
-idx2Discont = idx2_discont(eigenvector(:,:,1),eigenvector(:,:,2),...
-    resolution,angleTol);
+idx2Discont = idx2_discont(eigenvector(:,:,1),eigenvector(:,:,2),resolution,angleTol);
 deltaY = .5*diff(domain(2,:))/(double(resolution(2)) - 1);
-hDiscont = plot(a1,positionGridX(idx2Discont),...
-    positionGridY(idx2Discont)+deltaY,'ro');
+hDiscont = plot(hAxes,positionGridX(idx2Discont),positionGridY(idx2Discont)+deltaY,'ro');
 set(hDiscont,'MarkerFaceColor','r')
 
 function idx1Discont = idx1_discont(v1,v2,resolution,angleTol)
