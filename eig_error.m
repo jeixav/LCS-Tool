e0695c1ceef6a97296b9c2c81ad2bb970cc1244f
Kristjan Onu <kristjan.onu@mcgill.ca>
2013-02-27 11:30:50 -0500

    Modify calculation of eig error
    Testing shows new method gives smaller error

--- eig_error.m
+++ eig_error.m
@@ -5,7 +5,7 @@
 %
 % Description
 % The error is defined by:
-% e(i) = norm(a*v(:,i) - d(i)*v(:,i))
+% e(i) = norm((a - d(i,i)*eye(size(a)))*v(:,i))
 %
 % Example
 % a = rand(4);
@@ -15,7 +15,7 @@
 function EigError = eig_error(a,v,d)
 
     function EigErrorArrayfun = eig_error_arrayfun(idx,a,v,d)
-        EigErrorArrayfun = norm(a*v(:,idx) - d(idx,idx)*v(:,idx));
+        EigErrorArrayfun = norm((a - d(idx,idx)*eye(size(a)))*v(:,idx));
     end        
 
 EigError = arrayfun(@(idx)eig_error_arrayfun(idx,a,v,d),1:size(a,1));
