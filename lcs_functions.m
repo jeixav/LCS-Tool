bd4385f3b935b58c3b6b721ab9ec1f36dc4a2fa1
Kristjan Onu <kristjan.onu@mcgill.ca>
2013-03-19 08:36:29 -0400

    Remove outdated documentation

--- lcs_functions.m
+++
@@ -1,6 +0,0 @@
-%% Function Reference
-% Lagrangian Coherent Structures Toolbox
-%
-% * <animate_flow_help.html |animate_flow|> - Display flow animation
-% * <strain_lcs_script_help.html |strain_lcs_script|> - Compute and plot strainline LCSs
-% * <sym2fun_help.html |sym2fun|> - Make a regular MATLAB function from a symbolic function
