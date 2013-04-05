a3b48984b5f97a6d583945a1ac6d094345c53c0a
Kristjan Onu <kristjan.onu@mcgill.ca>
2013-04-05 12:09:42 -0400

    Use inputParser instead of set_default

--- shear_lcs_script.m
+++ shear_lcs_script.m
@@ -1,25 +1,33 @@
 % shear_lcs_script Compute and plot shear LCSs
 %
 % SYNTAX
+% [input,hAxes] = shear_lcs_script(input)
+% [input,hAxes] = shear_lcs_script(input,showPlot)
 % [input,hAxes] = shear_lcs_script(input,showPlot,verbose)
 
-function [input,hAxes] = shear_lcs_script(input,showPlot,verbose)
+function [input,hAxes] = shear_lcs_script(input,varargin)
 
 narginchk(1,3)
 
-verboseDefault.progress = true;
-verboseDefault.stats = true;
-if nargin < 3
-    verbose = [];
-end
-verbose = set_default(verbose,verboseDefault);
+p = inputParser;
+p.StructExpand = false;
+
+validationFcn = @(x) isstruct(x) && isfield(x,'flow') && isfield(x,'shearline');
+addRequired(p,'input',validationFcn)
 
 showPlotDefault.shearlinePosFiltered = true;
 showPlotDefault.shearlineNegFiltered = true;
-if nargin < 2
-    showPlot = [];
-end
-showPlot = set_default(showPlot,showPlotDefault);
+addOptional(p,'showPlot',showPlotDefault,@isstruct)
+
+verboseDefault.progress = true;
+verboseDefault.stats = true;
+addOptional(p,'verbose',verboseDefault,@isstruct)
+
+parse(p,input,varargin{:})
+
+input = p.Results.input;
+showPlot = p.Results.showPlot;
+verbose = p.Results.verbose;
 
 [input.flow,input.shearline] = compute_shear_lcs(input.flow,input.shearline,verbose);
 
