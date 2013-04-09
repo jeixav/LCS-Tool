e89444c1dd1238ab0948d643a4547ef2f63b7dbc
Kristjan Onu <kristjan.onu@mcgill.ca>
2013-04-09 14:49:24 -0400

    Account for possibility of NaNs

--- cgStrain_stats.m
+++ cgStrain_stats.m
@@ -1,12 +1,15 @@
 % cgStrain_stats Cauchy-Green strain tensor statistics
 %
-% SYNXTAX
+% SYNTAX
 % cgStrain_stats(cgStrain,cgStrainEigenvector,cgStrainEigenvalue)
 
 function cgStrain_stats(cgStrain,cgStrainEigenvector,cgStrainEigenvalue)
 
 %% Negative eigenvalues
 fprintf('Number of negative eigenvalues: %u.\n',numel(find(cgStrainEigenvalue(:) < 0)))
+
+nanIdx = isnan(cgStrainEigenvalue(:,1));
+fprintf('Number of NaN values: %u.\n',sum(nanIdx))
 
 %% Eigenvalue and eigenvector error
 n = size(cgStrain,3);
@@ -18,22 +21,24 @@ n = size(cgStrain,3);
 EigError = arrayfun(@(idx)cg_eig_error_arrayfun(idx,cgStrain,cgStrainEigenvector,cgStrainEigenvalue),1:n,'UniformOutput',false);
 EigError = cell2mat(transpose(EigError));
 
-fprintf('max(cgStrain*cgStrainEigenvector - cgStrainEigenvalue*cgStrainEigenvector):\n')
-fprintf('\t1: %g\n',max(EigError(:,1)))
-fprintf('\t2: %g\n',max(EigError(:,2)))
+fprintf('cgStrain*cgStrainEigenvector - cgStrainEigenvalue*cgStrainEigenvector:\n')
+fprintf('\tmin 1: %g\t 2: %g\n',min(EigError(:,1)),min(EigError(:,2)))
+fprintf('\tmax 1: %g\t 2: %g\n',max(EigError(:,1)),max(EigError(:,2)))
+fprintf('\tmean 1: %g\t 2: %g\n',mean(EigError(~nanIdx,1)),mean(EigError(~nanIdx,2)))
+fprintf('\tmedian 1: %g\t 2: %g\n',median(EigError(~nanIdx,1)),median(EigError(~nanIdx,2)))
 
 %% Product of eigenvalues
 prodCgStrainD = prod(cgStrainEigenvalue,2);
 
 o(1) = 1 - max(cgStrainEigenvalue(:,1));
 detCgStrain = arrayfun(@(idx)det(cgStrain(:,:,idx)),1:size(cgStrain,3));
-o(2) = mean(abs(detCgStrain-1));
+o(2) = mean(abs(detCgStrain(~nanIdx)-1));
 
 fprintf('lambda_1*lambda2:\n')
 fprintf('\tmin = %g\n',min(prodCgStrainD))
 fprintf('\tmax = %g\n',max(prodCgStrainD))
-fprintf('\tmean = %g\n',mean(prodCgStrainD))
-fprintf('\tmedian = %g\n',median(prodCgStrainD))
+fprintf('\tmean = %g\n',mean(prodCgStrainD(~nanIdx)))
+fprintf('\tmedian = %g\n',median(prodCgStrainD(~nanIdx)))
 fprintf('1 - max(lambda_1) = %g\n',o(1))
 fprintf('mean(abs(detCgStrain-1)) = %g\n',o(2))
 
