8fe82dc0b406eca8a4290ef0d1e54674565beb6d
Kristjan Onu <kristjan.onu@mcgill.ca>
2013-04-26 11:02:13 -0400

    Cosmetic change. Delete old comments.

--- plot_ftle.m
+++ plot_ftle.m
@@ -2,10 +2,6 @@ function plot_ftle(hAxes,flow,ftle)
 
 ftle = reshape(ftle,fliplr(flow.resolution));
 
-% nContour = 40;
-% contourMin = min(ftle(:));
-% contourMax = max(ftle(:));
-
 h = imagesc(flow.domain(1,:),flow.domain(2,:),ftle);
 set(h,'Parent',hAxes);
 set(h,'tag','ftle')
