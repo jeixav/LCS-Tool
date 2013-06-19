cfb55b6e898565a7039916bf17349289041d84d2
Kristjan Onu <kristjan.onu@mcgill.ca>
2013-06-19 18:49:36 -0400

    Remove unused function

--- shift_ic.m
+++
@@ -1,8 +0,0 @@
-% shift_ic Place all positions inside domain
-function vector = shift_ic(resolution,domain)
-
-delta = (domain(2) - domain(1))/double(resolution);
-
-vector = linspace(domain(1),domain(2),resolution+1);
-vector = vector + .5*delta;
-vector = vector(1:end-1);
