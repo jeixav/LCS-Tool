c7912eb1d96edd2df0c94503658921cc28f31ebd
Kristjan Onu <kristjan.onu@mcgill.ca>
2013-06-20 17:12:42 -0400

    Enlarge double gyre domain to obtain seed point at saddle

--- demo/double_gyre/hyperbolic_lcs.m
+++ demo/double_gyre/hyperbolic_lcs.m
@@ -5,11 +5,11 @@ omega = pi/5;
 doubleGyre.flow.imposeIncompressibility = true;
 doubleGyre.flow = set_flow_derivative(@(t,x,useEoV)derivative(t,x,useEoV,epsilon,amplitude,omega),doubleGyre.flow);
 
-doubleGyre.flow = set_flow_domain([0,2;0,1],doubleGyre.flow);
+doubleGyre.flow = set_flow_domain([-.1,2.1;-.05,1.05],doubleGyre.flow);
 doubleGyre.flow = set_flow_timespan([0,20],doubleGyre.flow);
-doubleGyre.flow = set_flow_resolution([501,251],doubleGyre.flow);
+doubleGyre.flow = set_flow_resolution([551,276],doubleGyre.flow);
 
-doubleGyre.strainline = set_strainline_max_length(5);
+doubleGyre.strainline = set_strainline_max_length(10);
 doubleGyre.strainline = set_strainline_ode_solver_options(odeset('relTol',1e-6),doubleGyre.strainline);
 
 localMaxDistance = 4/double(doubleGyre.flow.resolution(1)-1);
@@ -22,18 +22,19 @@ coupledIntegration = true;
 cgEigenvalue2 = reshape(cgEigenvalue(:,2),fliplr(doubleGyre.flow.resolution));
 cgEigenvector1 = reshape(cgEigenvector(:,1:2),[fliplr(doubleGyre.flow.resolution),2]);
 
-%% Plot λ₂ field
+%% Plot finite-time Lyapunov exponent
+ftle = compute_ftle(cgEigenvalue2,diff(doubleGyre.flow.timespan));
 hFigure = figure;
 hAxes = axes;
 set(hAxes,'parent',hFigure)
 set(hAxes,'NextPlot','add')
 set(hAxes,'box','on')
 set(hAxes,'DataAspectRatio',[1,1,1])
-hImagesc = imagesc(doubleGyre.flow.domain(1,:),doubleGyre.flow.domain(2,:),log(cgEigenvalue2));
+hImagesc = imagesc(doubleGyre.flow.domain(1,:),doubleGyre.flow.domain(2,:),ftle);
 set(hImagesc,'parent',hAxes)
 axis(hAxes,'tight')
 hColorbar = colorbar('peer',hAxes);
-set(get(hColorbar,'xlabel'),'string','log(\lambda_2)')
+set(get(hColorbar,'xlabel'),'string','FTLE')
 drawnow
 
 %% Compute strainlines
