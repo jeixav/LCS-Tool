b78abc0c8766d7ddb649f90d117bdf5851cccca1
Kristjan Onu <kristjan.onu@mcgill.ca>
2013-07-08 12:11:03 -0400

    Rename variables: strainline → curve

--- seed_curves_from_lambda_max.m
+++ seed_curves_from_lambda_max.m
@@ -1,27 +1,27 @@
-% seed_strainlines_from_lambda Seed strainlines from lambda maxima
+% seed_curves_from_lambda Seed curves from lambda maxima
 %
 % SYNTAX
-% [strainlinePosition,strainlineInitialPosition] = seed_strainlines_from_lambda(distance,cgEigenvalue,cgEigenvector,flowDomain)
-% [strainlinePosition,strainlineInitialPosition] = seed_strainlines_from_lambda(distance,cgEigenvalue,cgEigenvector,flowDomain,nMaxStrainlines)
+% [curvePosition,curveInitialPosition] = seed_curves_from_lambda(distance,cgEigenvalue,cgEigenvector,flowDomain)
+% [curvePosition,curveInitialPosition] = seed_curves_from_lambda(distance,cgEigenvalue,cgEigenvector,flowDomain,nMaxCurves)
 %
 % INPUT ARGUMENTS
-% distance: threshold distance for placement of lambda_2 maxima
-% nMaxStrainlines: Maximum number of hyperbolic LCSs to generate. Default
-% is numel(cgEigenvalue).
+% distance: threshold distance for placement of lambda maxima
+% nMaxCurves: Maximum number of curves to generate. Default is
+% numel(cgEigenvalue).
 
-function [strainlinePosition,strainlineInitialPosition] = seed_strainlines_from_lambda_max(distance,strainlineMaxLength,cgEigenvalue,cgEigenvector,flowDomain,varargin)
+function [curvePosition,curveInitialPosition] = seed_curves_from_lambda_max(distance,curveMaxLength,cgEigenvalue,cgEigenvector,flowDomain,varargin)
 
 narginchk(5,6)
 
 p = inputParser;
 addRequired(p,'distance',@(distance)validateattributes(distance,{'double'},{'scalar','>',0}))
-addRequired(p,'strainlineMaxLength',@(strainlineMaxLength)validateattributes(strainlineMaxLength,{'double'},{'scalar','>',0}))
+addRequired(p,'curveMaxLength',@(curveMaxLength)validateattributes(curveMaxLength,{'double'},{'scalar','>',0}))
 addRequired(p,'cgEigenvalue',@(cgEigenvalue)validateattributes(cgEigenvalue,{'double'},{'2d'}))
 
 % Must parse cgEigenvalue before proceeding
-parse(p,distance,strainlineMaxLength,cgEigenvalue)
+parse(p,distance,curveMaxLength,cgEigenvalue)
 distance = p.Results.distance;
-strainlineMaxLength = p.Results.strainlineMaxLength;
+curveMaxLength = p.Results.curveMaxLength;
 cgEigenvalue = p.Results.cgEigenvalue;
 flowResolution = fliplr(size(cgEigenvalue));
 
@@ -29,16 +29,16 @@ p = inputParser;
 addRequired(p,'cgEigenvector',@(cgEigenvector)validateattributes(cgEigenvector,{'double'},{'size',[fliplr(flowResolution),2]}))
 addRequired(p,'flowDomain',@(flowDomain)validateattributes(flowDomain,{'double'},{'size',[2,2]}))
 uint = {'uint8','uint16','uint32','uint64'};
-addOptional(p,'nMaxStrainlines',numel(cgEigenvalue),@(nMaxStrainlines)validateattributes(nMaxStrainlines,uint,{'scalar','>',0}));
+addOptional(p,'nMaxCurves',numel(cgEigenvalue),@(nMaxCurves)validateattributes(nMaxCurves,uint,{'scalar','>',0}));
 
 parse(p,cgEigenvector,flowDomain,varargin{:})
 
 cgEigenvector = p.Results.cgEigenvector;
 flowDomain = p.Results.flowDomain;
-nMaxStrainlines = p.Results.nMaxStrainlines;
+nMaxCurves = p.Results.nMaxCurves;
 
-%% Compute hyperbolic LCSs seeded from λ₂ local maxima
-% Array that records grid points where a strainline already exits
+%% Compute hyperbolic LCSs seeded from λ local maxima
+% Array that records grid points where a curve already exits
 flagArray = false(fliplr(flowResolution));
 
 gridSpace = diff(flowDomain(1,:))/(double(flowResolution(1))-1);
@@ -50,12 +50,12 @@ distanceGridPoints = uint64(distance./gridSpace);
 gridPosition{1} = linspace(flowDomain(1,1),flowDomain(1,2),flowResolution(1));
 gridPosition{2} = linspace(flowDomain(2,1),flowDomain(2,2),flowResolution(2));
 
-strainlinePosition = cell(1,nMaxStrainlines);
-strainlineInitialPosition = nan(2,nMaxStrainlines);
+curvePosition = cell(1,nMaxCurves);
+curveInitialPosition = nan(2,nMaxCurves);
 
 % Find all local maxima with a distance threshold
 [cgEigenvalueLocalMax,cgEigenvalueLocalMaxPosition] = local_max2D_gridded(cgEigenvalue,distanceGridPoints);
-nMaxStrainlines = min([nMaxStrainlines,numel(cgEigenvalueLocalMax)]);
+nMaxCurves = min([nMaxCurves,numel(cgEigenvalueLocalMax)]);
 [cgEigenvalueLocalMax,sortIndex] = sort(cgEigenvalueLocalMax,'descend');
 cgEigenvalueLocalMaxPosition = cgEigenvalueLocalMaxPosition(sortIndex,:);
 
@@ -65,78 +65,77 @@ cgEigenvectorInterpolant{2} = griddedInterpolant(fliplr(gridPosition),cgEigenvec
 % FIXME Should store cgEigenvector as m-by-n array or column array, not
 % both
 cgEigenvectorColumn = reshape(cgEigenvector,[numel(cgEigenvalue),2]);
-nStrainlines = 0;
+nCurves = 0;
 odeSolverOptions = odeset('relTol',1e-4);
 
-while nStrainlines < nMaxStrainlines
+while nCurves < nMaxCurves
     [nextLocalMax,loc] = find_next_local_max(cgEigenvalueLocalMax,cgEigenvalueLocalMaxPosition,flagArray);
     if isempty(nextLocalMax)
         break
     end
-    nStrainlines = nStrainlines + 1;
-    strainlineInitialPosition(:,nStrainlines) = [gridPosition{1}(loc(2)),gridPosition{2}(loc(1))];
+    nCurves = nCurves + 1;
+    curveInitialPosition(:,nCurves) = [gridPosition{1}(loc(2)),gridPosition{2}(loc(1))];
     periodicBc = false(2,1);
     % Event detection within integrate_line appears unreliable when initial
     % position is on domain boundary, therefore check if
-    % strainlineInitialPosition is on boundary before calling
-    % integrate_line.
+    % curveInitialPosition is on boundary before calling integrate_line.
     forwardTime = true;
     backwardTime = true;
-    if strainlineInitialPosition(1,nStrainlines) <= flowDomain(1,1)
-        if cgEigenvectorInterpolant{1}(fliplr(transpose(strainlineInitialPosition(:,nStrainlines)))) > 0
+    if curveInitialPosition(1,nCurves) <= flowDomain(1,1)
+        if cgEigenvectorInterpolant{1}(fliplr(transpose(curveInitialPosition(:,nCurves)))) > 0
             backwardTime = false;
         end
-        if cgEigenvectorInterpolant{1}(fliplr(transpose(strainlineInitialPosition(:,nStrainlines)))) < 0
+        if cgEigenvectorInterpolant{1}(fliplr(transpose(curveInitialPosition(:,nCurves)))) < 0
             forwardTime = false;
         end
     end
-    if strainlineInitialPosition(1,nStrainlines) >= flowDomain(1,2)
-        if cgEigenvectorInterpolant{1}(fliplr(transpose(strainlineInitialPosition(:,nStrainlines)))) > 0
+    if curveInitialPosition(1,nCurves) >= flowDomain(1,2)
+        if cgEigenvectorInterpolant{1}(fliplr(transpose(curveInitialPosition(:,nCurves)))) > 0
             forwardTime = false;
         end
-        if cgEigenvectorInterpolant{1}(fliplr(transpose(strainlineInitialPosition(:,nStrainlines)))) < 0
+        if cgEigenvectorInterpolant{1}(fliplr(transpose(curveInitialPosition(:,nCurves)))) < 0
             backwardTime = false;
         end
     end
-    if strainlineInitialPosition(2,nStrainlines) <= flowDomain(2,1)
-        if cgEigenvectorInterpolant{2}(fliplr(transpose(strainlineInitialPosition(:,nStrainlines)))) < 0
+    if curveInitialPosition(2,nCurves) <= flowDomain(2,1)
+        if cgEigenvectorInterpolant{2}(fliplr(transpose(curveInitialPosition(:,nCurves)))) < 0
             forwardTime = false;
         end
-        if cgEigenvectorInterpolant{2}(fliplr(transpose(strainlineInitialPosition(:,nStrainlines)))) > 0
+        if cgEigenvectorInterpolant{2}(fliplr(transpose(curveInitialPosition(:,nCurves)))) > 0
             backwardTime = false;
         end
     end
-    if strainlineInitialPosition(2,nStrainlines) >= flowDomain(2,2)
-        if cgEigenvectorInterpolant{2}(fliplr(transpose(strainlineInitialPosition(:,nStrainlines)))) > 0
+    if curveInitialPosition(2,nCurves) >= flowDomain(2,2)
+        if cgEigenvectorInterpolant{2}(fliplr(transpose(curveInitialPosition(:,nCurves)))) > 0
             forwardTime = false;
         end
-        if cgEigenvectorInterpolant{2}(fliplr(transpose(strainlineInitialPosition(:,nStrainlines)))) < 0
+        if cgEigenvectorInterpolant{2}(fliplr(transpose(curveInitialPosition(:,nCurves)))) < 0
             backwardTime = false;
         end
     end
     if forwardTime
-        positionPos = integrate_line([0,strainlineMaxLength],strainlineInitialPosition(:,nStrainlines),flowDomain,flowResolution,periodicBc,cgEigenvectorColumn,odeSolverOptions);
+        positionPos = integrate_line([0,curveMaxLength],curveInitialPosition(:,nCurves),flowDomain,flowResolution,periodicBc,cgEigenvectorColumn,odeSolverOptions);
     else
         positionPos = [];
     end
     if backwardTime
-        positionNeg = integrate_line([0,-strainlineMaxLength],strainlineInitialPosition(:,nStrainlines),flowDomain,flowResolution,periodicBc,cgEigenvectorColumn,odeSolverOptions);
+        positionNeg = integrate_line([0,-curveMaxLength],curveInitialPosition(:,nCurves),flowDomain,flowResolution,periodicBc,cgEigenvectorColumn,odeSolverOptions);
     else
         positionNeg = [];
     end
     % Remove duplicate point from forward and backward time integrations
     if isempty(positionNeg)
-        strainlinePosition{nStrainlines} = positionPos;
+        curvePosition{nCurves} = positionPos;
     else
-        strainlinePosition{nStrainlines} = [flipud(positionNeg);positionPos(2:end,:)];
+        curvePosition{nCurves} = [flipud(positionNeg);positionPos(2:end,:)];
     end
-    iFlagArray = line_grid_intersection(strainlinePosition{nStrainlines},gridPosition,gridSpace,distanceGridPoints);
+    iFlagArray = line_grid_intersection(curvePosition{nCurves},gridPosition,gridSpace,distanceGridPoints);
     flagArray = flagArray | iFlagArray;
 end
 
 % Remove unused elements
-strainlinePosition = strainlinePosition(~cellfun(@(input)isempty(input),strainlinePosition));
-strainlineInitialPosition = strainlineInitialPosition(:,1:nStrainlines);
+curvePosition = curvePosition(~cellfun(@(input)isempty(input),curvePosition));
+curveInitialPosition = curveInitialPosition(:,1:nCurves);
 
 function [nextLocalMax,nextPosition] = find_next_local_max(localMax,position,flagArray)
 
