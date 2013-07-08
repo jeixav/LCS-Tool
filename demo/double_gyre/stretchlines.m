b1f1757b00763c2b085a0e200a9fe1b44a2274ec
Kristjan Onu <kristjan.onu@mcgill.ca>
2013-07-08 11:07:56 -0400

    Seed stretchlines from λ₂ minima
    Seeding method seem poor since a large number of stretchlines are
    produced.

--- demo/double_gyre/stretchlines.m
+++ demo/double_gyre/stretchlines.m
@@ -8,10 +8,13 @@ timespan = 20;
 
 doubleGyre.flow = set_flow_domain([-.1,2.1;-.05,1.05],doubleGyre.flow);
 doubleGyre.flow = set_flow_timespan([0,timespan],doubleGyre.flow);
-doubleGyre.flow = set_flow_resolution([1101,551],doubleGyre.flow);
+doubleGyre.flow = set_flow_resolution([551,276],doubleGyre.flow);
 
 doubleGyre.strainline = set_strainline_max_length(20);
 doubleGyre.strainline = set_strainline_ode_solver_options(odeset('relTol',1e-6),doubleGyre.strainline);
+
+gridSpace = diff(doubleGyre.flow.domain(1,:))/(double(doubleGyre.flow.resolution(1))-1);
+localMaxDistance = 2*gridSpace;
 
 %% Compute Cauchy-Green strain eigenvalues and eigenvectors
 method.name = 'finiteDifference';
@@ -34,20 +37,11 @@ drawnow
 doubleGyre.stretchline.maxLength = 2;
 doubleGyre.stretchline.resolution = uint64([20,10]);
 doubleGyre.stretchline.odeSolverOptions = odeset('RelTol',1e-6);
-doubleGyre.stretchline.position = compute_stretchline(doubleGyre.flow,doubleGyre.stretchline);
+[doubleGyre.stretchline.position,doubleGyre.stretchline.initialPosition] = seed_curves_from_lambda_max(localMaxDistance,doubleGyre.stretchline.maxLength,-cgEigenvalue(:,:,2),cgEigenvector(:,:,3:4),doubleGyre.flow.domain);
 % Plot stretchlines
 hStretchline = cellfun(@(position)plot(hAxes,position(:,1),position(:,2)),doubleGyre.stretchline.position);
-grayColor = [.7,.7,.7];
-set(hStretchline,'color',grayColor)
-% Calculate relative stretching
-segmentIndex = cellfun(@(position)[1,size(position,1)],doubleGyre.stretchline.position,'UniformOutput',false);
-gridPosition = initialize_ic_grid(doubleGyre.flow.resolution,doubleGyre.flow.domain);
-relativeStretching = relative_stretching(doubleGyre.stretchline.position,segmentIndex,doubleGyre.flow.cgEigenvalue(:,2),doubleGyre.flow.domain,doubleGyre.flow.resolution,false);
-relativeStretching = cell2mat(relativeStretching);
-
-[~,sortIndex] = sort(relativeStretching);
-
-% Highlight most-stretching stretchlines
-nMost = 20;
-hStretchlineMost = cellfun(@(position)plot(hAxes,position(:,1),position(:,2)),doubleGyre.stretchline.position(sortIndex(end-nMost:end)));
-set(hStretchlineMost,'color','r')
+set(hStretchline,'color','r')
+hStrainlineInitialPosition = arrayfun(@(idx)plot(hAxes,doubleGyre.stretchline.initialPosition(1,idx),doubleGyre.stretchline.initialPosition(2,idx)),1:numel(doubleGyre.stretchline.position));
+set(hStrainlineInitialPosition,'marker','o')
+set(hStrainlineInitialPosition,'MarkerEdgeColor','k')
+set(hStrainlineInitialPosition,'MarkerFaceColor','k')
