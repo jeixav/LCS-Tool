94dc861df823e2424379c5f6b204f0e319c3acf9
Kristjan Onu <kristjan.onu@mcgill.ca>
2013-07-31 11:32:55 -0400

    Update stretchline double gyre and Bickley jet demos
    Simplify demos to perform uniform stretchline seeding. Remove relative
    stretching and seeding based on λ minima. Compute attracting and
    repelling structures.

--- demo/bickley_jet/stretchlines.m
+++ demo/bickley_jet/stretchlines.m
@@ -16,43 +16,31 @@ bickleyJet.flow = set_flow_resolution([500,200],bickleyJet.flow);
 % magicNumber = 2.2599.
 magicNumber = .5*pi*earthRadius/lengthY*double(bickleyJet.flow.resolution(2)-1)/double(bickleyJet.flow.resolution(1)-1);
 bickleyJet.flow = set_flow_domain([0,lengthX;[-1,1]*magicNumber*lengthY],bickleyJet.flow);
-bickleyJet.flow = set_flow_timespan([0,4*lengthX/u],bickleyJet.flow);
+timespan = 4*lengthX/u;
 
-%% Compute Cauchy-Green strain eigenvalues and eigenvectors
-method.name = 'finiteDifference';
-customEigMethod = false;
-coupledIntegration = true;
-[bickleyJet.flow.cgEigenvalue,bickleyJet.flow.cgEigenvector] = eig_cgStrain(bickleyJet.flow,method,customEigMethod,coupledIntegration);
-cgEigenvalue = reshape(bickleyJet.flow.cgEigenvalue,[fliplr(bickleyJet.flow.resolution),2]);
-cgEigenvector = reshape(bickleyJet.flow.cgEigenvector,[fliplr(bickleyJet.flow.resolution),4]);
-
-% Plot finite-time Lyapunov exponent
-ftle = compute_ftle(cgEigenvalue(:,:,2),diff(bickleyJet.flow.timespan));
-hAxes = setup_figure(bickleyJet.flow.domain);
-hImagesc = imagesc(bickleyJet.flow.domain(1,:),bickleyJet.flow.domain(2,:),ftle);
-set(hImagesc,'parent',hAxes)
-hColorbar = colorbar('peer',hAxes);
-set(get(hColorbar,'xlabel'),'string','FTLE')
-drawnow
-
-%% Compute stretchlines
 bickleyJet.stretchline.maxLength = 1e7;
 bickleyJet.stretchline.resolution = uint64([20,10]);
 bickleyJet.stretchline.odeSolverOptions = odeset('RelTol',1e-6);
+
+% Cauchy-Green strain numerical parameters
+method.name = 'finiteDifference';
+customEigMethod = false;
+coupledIntegration = true;
+
+%% Attracting stretchlines
+bickleyJet.flow = set_flow_timespan([0,4*lengthX/u],bickleyJet.flow);
+[bickleyJet.flow.cgEigenvalue,bickleyJet.flow.cgEigenvector] = eig_cgStrain(bickleyJet.flow,method,customEigMethod,coupledIntegration);
 bickleyJet.stretchline.position = compute_stretchline(bickleyJet.flow,bickleyJet.stretchline);
-% Plot stretchlines
+hAxes = setup_figure(bickleyJet.flow.domain);
+cellfun(@(position)plot(hAxes,position(:,1),position(:,2)),bickleyJet.stretchline.position);
+title(hAxes,'Attracting Stretchlines')
+drawnow
+
+%% Repelling stretchlines
+bickleyJet.flow = set_flow_timespan([4*lengthX/u,0],bickleyJet.flow);
+[bickleyJet.flow.cgEigenvalue,bickleyJet.flow.cgEigenvector] = eig_cgStrain(bickleyJet.flow,method,customEigMethod,coupledIntegration);
+bickleyJet.stretchline.position = compute_stretchline(bickleyJet.flow,bickleyJet.stretchline);
+hAxes = setup_figure(bickleyJet.flow.domain);
 hStretchline = cellfun(@(position)plot(hAxes,position(:,1),position(:,2)),bickleyJet.stretchline.position);
-grayColor = [.7,.7,.7];
-set(hStretchline,'color',grayColor)
-% Calculate relative stretching
-segmentIndex = cellfun(@(position)[1,size(position,1)],bickleyJet.stretchline.position,'UniformOutput',false);
-gridPosition = initialize_ic_grid(bickleyJet.flow.resolution,bickleyJet.flow.domain);
-relativeStretching = relative_stretching(bickleyJet.stretchline.position,segmentIndex,bickleyJet.flow.cgEigenvalue(:,2),bickleyJet.flow.domain,bickleyJet.flow.resolution,false);
-relativeStretching = cell2mat(relativeStretching);
-
-[~,sortIndex] = sort(relativeStretching);
-
-% Highlight most-stretching stretchlines
-nMost = 50;
-hStretchlineMost = cellfun(@(position)plot(hAxes,position(:,1),position(:,2)),bickleyJet.stretchline.position(sortIndex(end-nMost:end)));
-set(hStretchlineMost,'color','r')
+set(hStretchline,'color','r')
+title(hAxes,'Repeling Stretchlines')
--- demo/double_gyre/stretchlines.m
+++ demo/double_gyre/stretchlines.m
@@ -5,61 +5,32 @@ omega = pi/5;
 doubleGyre.flow.imposeIncompressibility = true;
 doubleGyre.flow = set_flow_derivative(@(t,x,useEoV)derivative(t,x,useEoV,epsilon,amplitude,omega),doubleGyre.flow);
 timespan = 20;
-
 doubleGyre.flow = set_flow_domain([-.1,2.1;-.05,1.05],doubleGyre.flow);
-doubleGyre.flow = set_flow_timespan([0,timespan],doubleGyre.flow);
 doubleGyre.flow = set_flow_resolution([551,276],doubleGyre.flow);
 
-doubleGyre.strainline = set_strainline_max_length(20);
-doubleGyre.strainline = set_strainline_ode_solver_options(odeset('relTol',1e-6),doubleGyre.strainline);
-
-%% Compute Cauchy-Green strain eigenvalues and eigenvectors
-method.name = 'finiteDifference';
-customEigMethod = false;
-coupledIntegration = true;
-[doubleGyre.flow.cgEigenvalue,doubleGyre.flow.cgEigenvector] = eig_cgStrain(doubleGyre.flow,method,customEigMethod,coupledIntegration);
-cgEigenvalue = reshape(doubleGyre.flow.cgEigenvalue,[fliplr(doubleGyre.flow.resolution),2]);
-cgEigenvector = reshape(doubleGyre.flow.cgEigenvector,[fliplr(doubleGyre.flow.resolution),4]);
-
-% Plot finite-time Lyapunov exponent
-ftle = compute_ftle(cgEigenvalue(:,:,2),diff(doubleGyre.flow.timespan));
-hAxes = setup_figure(doubleGyre.flow.domain);
-hImagesc = imagesc(doubleGyre.flow.domain(1,:),doubleGyre.flow.domain(2,:),ftle);
-set(hImagesc,'parent',hAxes)
-hColorbar = colorbar('peer',hAxes);
-set(get(hColorbar,'xlabel'),'string','FTLE')
-drawnow
-
-%% Uniform stretchline seeding, then filtering based on relative stretching
 doubleGyre.stretchline.maxLength = 2;
 doubleGyre.stretchline.resolution = uint64([20,10]);
 doubleGyre.stretchline.odeSolverOptions = odeset('RelTol',1e-6);
+
+% Cauchy-Green strain numerical parameters
+method.name = 'finiteDifference';
+customEigMethod = false;
+coupledIntegration = true;
+
+%% Attracting stretchlines
+doubleGyre.flow = set_flow_timespan([0,timespan],doubleGyre.flow);
+[doubleGyre.flow.cgEigenvalue,doubleGyre.flow.cgEigenvector] = eig_cgStrain(doubleGyre.flow,method,customEigMethod,coupledIntegration);
 doubleGyre.stretchline.position = compute_stretchline(doubleGyre.flow,doubleGyre.stretchline);
+hAxes = setup_figure(doubleGyre.flow.domain);
+cellfun(@(position)plot(hAxes,position(:,1),position(:,2)),doubleGyre.stretchline.position);
+title(hAxes,'Attracting Stretchlines')
+drawnow
+
+%% Repelling stretchlines
+doubleGyre.flow = set_flow_timespan([timespan,0],doubleGyre.flow);
+[doubleGyre.flow.cgEigenvalue,doubleGyre.flow.cgEigenvector] = eig_cgStrain(doubleGyre.flow,method,customEigMethod,coupledIntegration);
+doubleGyre.stretchline.position = compute_stretchline(doubleGyre.flow,doubleGyre.stretchline);
+hAxes = setup_figure(doubleGyre.flow.domain);
 hStretchline = cellfun(@(position)plot(hAxes,position(:,1),position(:,2)),doubleGyre.stretchline.position);
-grayBlue = [.7,.7,.8];
-set(hStretchline,'color',grayBlue)
-
-% Calculate relative stretching
-segmentIndex = cellfun(@(position)[1,size(position,1)],doubleGyre.stretchline.position,'UniformOutput',false);
-gridPosition = initialize_ic_grid(doubleGyre.flow.resolution,doubleGyre.flow.domain);
-relativeStretching = relative_stretching(doubleGyre.stretchline.position,segmentIndex,doubleGyre.flow.cgEigenvalue(:,2),doubleGyre.flow.domain,doubleGyre.flow.resolution,false);
-relativeStretching = cell2mat(relativeStretching);
-
-[~,sortIndex] = sort(relativeStretching);
-% Highlight most-stretching stretchlines
-nMost = 20;
-hStretchlineMost = cellfun(@(position)plot(hAxes,position(:,1),position(:,2)),doubleGyre.stretchline.position(sortIndex(end-nMost+1:end)));
-
-%% Seed stretchlines at λ₂ maxima
-gridSpace = diff(doubleGyre.flow.domain(1,:))/(double(doubleGyre.flow.resolution(1))-1);
-localMaxDistance = 10*gridSpace;
-[doubleGyre.stretchline.position,doubleGyre.stretchline.initialPosition] = seed_curves_from_lambda_max(localMaxDistance,doubleGyre.stretchline.maxLength,cgEigenvalue(:,:,2),cgEigenvector(:,:,3:4),doubleGyre.flow.domain);
-% Plot stretchlines
-hStretchline = cellfun(@(position)plot(hAxes,position(:,1),position(:,2)),doubleGyre.stretchline.position);
-grayBlue = [.45,.45,1];
-set(hStretchline,'color',grayBlue)
-hStretchlineInitialPosition = arrayfun(@(idx)plot(hAxes,doubleGyre.stretchline.initialPosition(1,idx),doubleGyre.stretchline.initialPosition(2,idx)),1:numel(doubleGyre.stretchline.position));
-set(hStretchlineInitialPosition,'marker','o')
-set(hStretchlineInitialPosition,'MarkerEdgeColor','w')
-set(hStretchlineInitialPosition,'MarkerFaceColor',grayBlue)
-set(hStretchlineInitialPosition,'MarkerSize',3)
+set(hStretchline,'color','r')
+title(hAxes,'Repeling Stretchlines')
