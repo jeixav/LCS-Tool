1a72fe060f8e430fff9c3f35d054afba4807e056
Kristjan Onu <kristjan.onu@mcgill.ca>
2013-08-29 11:58:36 -0400

    Remove outdated custom HTML documentation

--- lcs_product_page.m
+++
@@ -1,7 +0,0 @@
-%% Lagrangian Coherent Structures Toolbox
-%
-% *Available Documentation*
-%
-% * <lcs_user_guide.html User's Guide>
-%
-% Copyright 2012,2013 Kristjan Onu
--- publish_help.m
+++
@@ -1,8 +0,0 @@
-function publish_help
-%PUBLISH_HELP Create LCS Toolbox help browser documentation
-
-publish('lcs_user_guide.m','evalCode',false);
-
-publish('lcs_product_page.m','evalCode',false);
-
-end
