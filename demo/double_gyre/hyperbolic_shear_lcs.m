24f41574c7678cf57f34bdddc0ca69961bff155e
Kristjan Onu <jeixav@gmail.com>
2013-10-18 17:09:10 -0400

    Analyze η₊ and η₋ when detecting closed orbits in double gyre demo

--- demo/double_gyre/hyperbolic_shear_lcs.m
+++ demo/double_gyre/hyperbolic_shear_lcs.m
@@ -1,20 +1,19 @@
+function hyperbolic_shear_lcs
+
 %% Input parameters
 epsilon = .1;
 amplitude = .1;
 omega = pi/5;
-doubleGyre.flow.imposeIncompressibility = true;
-doubleGyre.flow = set_flow_derivative(@(t,x,useEoV)derivative(t,x,useEoV,epsilon,amplitude,omega),doubleGyre.flow);
-timespan = 20;
 
-doubleGyre.flow = set_flow_domain([-.1,2.1;-.05,1.05],doubleGyre.flow);
-doubleGyre.flow = set_flow_timespan([0,timespan],doubleGyre.flow);
-doubleGyre.flow = set_flow_resolution([551,276],doubleGyre.flow);
-doubleGyre.flow.periodicBc = [false,false];
+flow.imposeIncompressibility = true;
+flow.derivative = @(t,x,useEoV)derivative(t,x,useEoV,epsilon,amplitude,omega);
+flow.domain = [-.1,2.1;-.05,1.05];
+flow.timespan = [0,20];
+flow.resolution = [551,276];
 
-doubleGyre.strainline = set_strainline_max_length(20);
-doubleGyre.strainline = set_strainline_ode_solver_options(odeset('relTol',1e-6),doubleGyre.strainline);
+strainlineMaxLength = 20;
 
-gridSpace = diff(doubleGyre.flow.domain(1,:))/(double(doubleGyre.flow.resolution(1))-1);
+gridSpace = diff(flow.domain(1,:))/(double(flow.resolution(1))-1);
 localMaxDistance = 2*gridSpace;
 
 forwardLcsColor = 'r';
@@ -23,51 +22,54 @@ shearLcsColor = [0,.6,0];
 
 %% Forward-time LCS analysis
 % Compute Cauchy-Green strain eigenvalues and eigenvectors
-[doubleGyre.flow.cgEigenvalue,doubleGyre.flow.cgEigenvector] = eig_cgStrain(doubleGyre.flow);
+[flow.cgEigenvalue,flow.cgEigenvector] = eig_cgStrain(flow);
 % FIXME Should use m-by-n or (m*n)-by-2 array forms throughout LCS Tool
-cgEigenvalue = reshape(doubleGyre.flow.cgEigenvalue,[fliplr(doubleGyre.flow.resolution),2]);
-cgEigenvector = reshape(doubleGyre.flow.cgEigenvector,[fliplr(doubleGyre.flow.resolution),4]);
+cgEigenvalue = reshape(flow.cgEigenvalue,[fliplr(flow.resolution),2]);
+cgEigenvector = reshape(flow.cgEigenvector,[fliplr(flow.resolution),4]);
+
+% Define Poincare sections; first point in center of elliptic region and
+% second point outside elliptic region
+poincareSection = struct('endPosition',{},'numPoints',{},'orbitMaxLength',{});
+
+poincareSection(1).endPosition = [.5,.6;.35,.5];
+poincareSection(2).endPosition = [1.5,.4;1.7,.5];
+
+% Number of orbit seed points along each Poincare section
+[poincareSection.numPoints] = deal(80);
+
+% Set maximum orbit length to twice the expected circumference
+nPoincareSection = numel(poincareSection);
+for i = 1:nPoincareSection
+    rOrbit = hypot(diff(poincareSection(i).endPosition(:,1)),diff(poincareSection(i).endPosition(:,2)));
+    poincareSection(i).orbitMaxLength = 2*(2*pi*rOrbit);
+end
+
+[shearline.etaPos,shearline.etaNeg] = lagrangian_shear(flow.cgEigenvector,flow.cgEigenvalue);
 
 % Compute closed shearlines
-% Define Poincare sections
-% Place first point in center of elliptic region and second point outside
-% elliptic region
-poincareSection{1}.endPosition = [.5,.6;.35,.5];
-poincareSection{1}.numPoints = 80;
-% set maximum integration length to twice the expected circumference
-rOrbit = hypot(diff(poincareSection{1}.endPosition(:,1)),diff(poincareSection{1}.endPosition(:,2)));
-poincareSection{1}.integrationLength = [0,2*(2*pi*rOrbit)];
+closedOrbits = poincare_closed_orbit_multi(flow,shearline,poincareSection);
 
-% Find closed orbits with Poincare section return map
-[etaPos,etaNeg] = lagrangian_shear(doubleGyre.flow.cgEigenvector,doubleGyre.flow.cgEigenvalue);
-odeSolverOptions = odeset('relTol',1e-3);
-nBisection = 2;
-dThresh = 1e-2;
-closedOrbitPos = poincare_closed_orbit(doubleGyre.flow,etaPos,poincareSection{1},odeSolverOptions,nBisection,dThresh);
-% Discard all but outermost closed orbit
-closedOrbitPos = closedOrbitPos{end};
-
-% Plot closed orbit
-hAxes = setup_figure(doubleGyre.flow.domain);
+% Plot elliptic LCS
+hAxes = setup_figure(flow.domain);
 title(hAxes,'Forward-time LCS')
-hClosedOrbit = plot(hAxes,closedOrbitPos(:,1),closedOrbitPos(:,2));
-set(hClosedOrbit,'color',shearLcsColor)
-drawnow
-
-% Repeat for second Poincare section, with etaNeg vector
-poincareSection{1}.endPosition = [1.5,.4;1.7,.5];
-rOrbit = hypot(diff(poincareSection{1}.endPosition(:,1)),diff(poincareSection{1}.endPosition(:,2)));
-poincareSection{1}.integrationLength = [0,2*(2*pi*rOrbit)];
-closedOrbitNeg = poincare_closed_orbit(doubleGyre.flow,etaNeg,poincareSection{1},odeSolverOptions,nBisection,dThresh);
-closedOrbitNeg = closedOrbitNeg{end};
-hClosedOrbit = plot(hAxes,closedOrbitNeg(:,1),closedOrbitNeg(:,2));
-set(hClosedOrbit,'color',shearLcsColor)
+% η₊ outermost closed orbit
+hClosedOrbitsEtaPos = arrayfun(@(i)plot(hAxes,closedOrbits{i}{1}{end}(:,1),closedOrbits{i}{1}{end}(:,2)),1:size(closedOrbits,2));
+set(hClosedOrbitsEtaPos,'color',shearLcsColor)
+set(hClosedOrbitsEtaPos,'linewidth',2)
+% η₋ outermost closed orbit
+hClosedOrbitsEtaNeg = arrayfun(@(i)plot(hAxes,closedOrbits{i}{2}{end}(:,1),closedOrbits{i}{2}{end}(:,2)),1:size(closedOrbits,2));
+set(hClosedOrbitsEtaNeg,'color',shearLcsColor)
+set(hClosedOrbitsEtaNeg,'linewidth',2)
 drawnow
 
 % Compute strainlines
-strainlinePosition = seed_curves_from_lambda_max(localMaxDistance,doubleGyre.strainline.maxLength,cgEigenvalue(:,:,2),cgEigenvector(:,:,1:2),doubleGyre.flow.domain,doubleGyre.flow.periodicBc);
-strainlinePosition = remove_strain_in_shear(strainlinePosition,closedOrbitPos);
-strainlinePosition = remove_strain_in_shear(strainlinePosition,closedOrbitNeg);
+strainlinePosition = seed_curves_from_lambda_max(localMaxDistance,strainlineMaxLength,cgEigenvalue(:,:,2),cgEigenvector(:,:,1:2),flow.domain);
+
+for i = 1:nPoincareSection
+    % Remove strainlines inside of ellitpic regions
+    strainlinePosition = remove_strain_in_shear(strainlinePosition,closedOrbits{i}{1}{end});
+    strainlinePosition = remove_strain_in_shear(strainlinePosition,closedOrbits{i}{2}{end});
+end
 
 % Plot strainlines
 hStrainline = cellfun(@(position)plot(hAxes,position(:,1),position(:,2)),strainlinePosition);
@@ -76,49 +78,51 @@ drawnow
 
 %% Backward-time LCS analysis
 % Compute Cauchy-Green strain eigenvalues and eigenvectors
-doubleGyreBackward = doubleGyre;
-doubleGyreBackward.flow = set_flow_timespan([timespan,0],doubleGyre.flow);
-[doubleGyreBackward.flow.cgEigenvalue,doubleGyreBackward.flow.cgEigenvector] = eig_cgStrain(doubleGyreBackward.flow);
-cgEigenvalue = reshape(doubleGyreBackward.flow.cgEigenvalue,[fliplr(doubleGyreBackward.flow.resolution),2]);
-cgEigenvector = reshape(doubleGyreBackward.flow.cgEigenvector,[fliplr(doubleGyreBackward.flow.resolution),4]);
+flow.timespan = fliplr(flow.timespan);
+[flow.cgEigenvalue,flow.cgEigenvector] = eig_cgStrain(flow);
+cgEigenvalue = reshape(flow.cgEigenvalue,[fliplr(flow.resolution),2]);
+cgEigenvector = reshape(flow.cgEigenvector,[fliplr(flow.resolution),4]);
+
+% Define Poincare sections
+poincareSection(1).endPosition = [.5,.6;.35,.5];
+poincareSection(2).endPosition = [1.5,.4;1.7,.5];
+
+% Number of orbit seed points along each Poincare section
+[poincareSection.numPoints] = deal(80);
+
+% Set maximum orbit length to twice the expected circumference
+nPoincareSection = numel(poincareSection);
+for i = 1:nPoincareSection
+    rOrbit = hypot(diff(poincareSection(i).endPosition(:,1)),diff(poincareSection(i).endPosition(:,2)));
+    poincareSection(i).orbitMaxLength = 2*(2*pi*rOrbit);
+end
+
+[shearline.etaPos,shearline.etaNeg] = lagrangian_shear(flow.cgEigenvector,flow.cgEigenvalue);
 
 % Compute closed shearlines
-% Define Poincare sections
-% Place first point in center of elliptic region and second point outside
-% elliptic region
-poincareSection{1}.endPosition = [.5,.6;.3,.5];
-poincareSection{1}.numPoints = 80;
-rOrbit = hypot(diff(poincareSection{1}.endPosition(:,1)),diff(poincareSection{1}.endPosition(:,2)));
-poincareSection{1}.integrationLength = [0,2*(2*pi*rOrbit)];
+closedOrbits = poincare_closed_orbit_multi(flow,shearline,poincareSection);
 
-% Find closed orbits with Poincare section return map
-[etaPos,etaNeg] = lagrangian_shear(doubleGyreBackward.flow.cgEigenvector,doubleGyreBackward.flow.cgEigenvalue);
-odeSolverOptions = odeset('relTol',1e-3);
-nBisection = 2;
-dThresh = 1e-2;
-closedOrbitPos = poincare_closed_orbit(doubleGyreBackward.flow,etaNeg,poincareSection{1},odeSolverOptions,nBisection,dThresh);
-closedOrbitPos = closedOrbitPos{end};
-
-hAxes = setup_figure(doubleGyreBackward.flow.domain);
+% Plot elliptic LCS
+hAxes = setup_figure(flow.domain);
 title(hAxes,'Backward-time LCS')
-hClosedOrbit = plot(hAxes,closedOrbitPos(:,1),closedOrbitPos(:,2));
-set(hClosedOrbit,'color',shearLcsColor)
-drawnow
-
-% Repeat for second Poincare section, with etaPos vector
-poincareSection{1}.endPosition = [1.5,.4;1.7,.5];
-rOrbit = hypot(diff(poincareSection{1}.endPosition(:,1)),diff(poincareSection{1}.endPosition(:,2)));
-poincareSection{1}.integrationLength = [0,2*(2*pi*rOrbit)];
-closedOrbitNeg = poincare_closed_orbit(doubleGyre.flow,etaPos,poincareSection{1},odeSolverOptions,nBisection,dThresh);
-closedOrbitNeg = closedOrbitNeg{end};
-hClosedOrbit = plot(hAxes,closedOrbitNeg(:,1),closedOrbitNeg(:,2));
-set(hClosedOrbit,'color',shearLcsColor)
+% η₊ outermost closed orbit
+hClosedOrbitsEtaPos = arrayfun(@(i)plot(hAxes,closedOrbits{i}{1}{end}(:,1),closedOrbits{i}{1}{end}(:,2)),1:size(closedOrbits,2));
+set(hClosedOrbitsEtaPos,'color',shearLcsColor)
+set(hClosedOrbitsEtaPos,'linewidth',2)
+% η₋ outermost closed orbit
+hClosedOrbitsEtaNeg = arrayfun(@(i)plot(hAxes,closedOrbits{i}{2}{end}(:,1),closedOrbits{i}{2}{end}(:,2)),1:size(closedOrbits,2));
+set(hClosedOrbitsEtaNeg,'color',shearLcsColor)
+set(hClosedOrbitsEtaNeg,'linewidth',2)
 drawnow
 
 % Compute strainlines
-[strainlinePosition] = seed_curves_from_lambda_max(localMaxDistance,doubleGyreBackward.strainline.maxLength,cgEigenvalue(:,:,2),cgEigenvector(:,:,1:2),doubleGyreBackward.flow.domain,doubleGyreBackward.flow.periodicBc);
-strainlinePosition = remove_strain_in_shear(strainlinePosition,closedOrbitPos);
-strainlinePosition = remove_strain_in_shear(strainlinePosition,closedOrbitNeg);
+strainlinePosition = seed_curves_from_lambda_max(localMaxDistance,strainlineMaxLength,cgEigenvalue(:,:,2),cgEigenvector(:,:,1:2),flow.domain);
+
+for i = 1:nPoincareSection
+    % Remove strainlines inside of ellitpic regions
+    strainlinePosition = remove_strain_in_shear(strainlinePosition,closedOrbits{i}{1}{end});
+    strainlinePosition = remove_strain_in_shear(strainlinePosition,closedOrbits{i}{2}{end});
+end
 
 % Plot strainlines
 hStrainline = cellfun(@(position)plot(hAxes,position(:,1),position(:,2)),strainlinePosition);
--- demo/double_gyre/hyperbolic_shear_lcs_details.m
+++ demo/double_gyre/hyperbolic_shear_lcs_details.m
@@ -2,19 +2,17 @@
 epsilon = .1;
 amplitude = .1;
 omega = pi/5;
-doubleGyre.flow.imposeIncompressibility = true;
-doubleGyre.flow = set_flow_derivative(@(t,x,useEoV)derivative(t,x,useEoV,epsilon,amplitude,omega),doubleGyre.flow);
+flow.imposeIncompressibility = true;
+flow.derivative = @(t,x,useEoV)derivative(t,x,useEoV,epsilon,amplitude,omega);
 timespan = 20;
 
-doubleGyre.flow = set_flow_domain([-.1,2.1;-.05,1.05],doubleGyre.flow);
-doubleGyre.flow = set_flow_timespan([0,timespan],doubleGyre.flow);
-doubleGyre.flow = set_flow_resolution([551,276],doubleGyre.flow);
-doubleGyre.flow.periodicBc = [false,false];
+flow.domain = [-.1,2.1;-.05,1.05];
+flow.timespan = [0,timespan];
+flow.resolution = [551,276];
 
-doubleGyre.strainline = set_strainline_max_length(20);
-doubleGyre.strainline = set_strainline_ode_solver_options(odeset('relTol',1e-6),doubleGyre.strainline);
+strainlineMaxLength = 20;
 
-gridSpace = diff(doubleGyre.flow.domain(1,:))/(double(doubleGyre.flow.resolution(1))-1);
+gridSpace = diff(flow.domain(1,:))/(double(flow.resolution(1))-1);
 localMaxDistance = 2*gridSpace;
 
 forwardLcsColor = 'r';
@@ -23,71 +21,84 @@ shearLcsColor = [0,.6,0];
 
 %% Forward-time LCS analysis
 % Compute Cauchy-Green strain eigenvalues and eigenvectors
-[doubleGyre.flow.cgEigenvalue,doubleGyre.flow.cgEigenvector] = eig_cgStrain(doubleGyre.flow);
+[flow.cgEigenvalue,flow.cgEigenvector] = eig_cgStrain(flow);
 % FIXME Should use m-by-n or (m*n)-by-2 array forms throughout LCS Tool
-cgEigenvalue = reshape(doubleGyre.flow.cgEigenvalue,[fliplr(doubleGyre.flow.resolution),2]);
-cgEigenvector = reshape(doubleGyre.flow.cgEigenvector,[fliplr(doubleGyre.flow.resolution),4]);
+cgEigenvalue = reshape(flow.cgEigenvalue,[fliplr(flow.resolution),2]);
+cgEigenvector = reshape(flow.cgEigenvector,[fliplr(flow.resolution),4]);
 
 % Plot finite-time Lyapunov exponent
-ftle = compute_ftle(cgEigenvalue(:,:,2),diff(doubleGyre.flow.timespan));
-hAxes = setup_figure(doubleGyre.flow.domain);
+ftle = compute_ftle(cgEigenvalue(:,:,2),diff(flow.timespan));
+hAxes = setup_figure(flow.domain);
 title(hAxes,'Forward-time LCS')
-plot_ftle(hAxes,doubleGyre.flow,ftle);
+plot_ftle(hAxes,flow,ftle);
 colormap(hAxes,flipud(gray))
 drawnow
 
+% Define Poincare sections; first point in center of elliptic region and
+% second point outside elliptic region
+poincareSection = struct('endPosition',{},'numPoints',{},'orbitMaxLength',{});
+
+poincareSection(1).endPosition = [.5,.6;.35,.5];
+poincareSection(2).endPosition = [1.5,.4;1.7,.5];
+
+% Plot Poincare sections
+hPoincareSection = arrayfun(@(input)plot(hAxes,input.endPosition(:,1),input.endPosition(:,2)),poincareSection);
+set(hPoincareSection,'color',shearLcsColor)
+set(hPoincareSection,'LineStyle','--')
+set(hPoincareSection,'marker','o')
+set(hPoincareSection,'MarkerFaceColor',shearLcsColor)
+set(hPoincareSection,'MarkerEdgeColor','w')
+drawnow
+
+% Number of orbit seed points along each Poincare section
+[poincareSection.numPoints] = deal(80);
+
+% Set maximum orbit length to twice the expected circumference
+nPoincareSection = numel(poincareSection);
+for i = 1:nPoincareSection
+    rOrbit = hypot(diff(poincareSection(i).endPosition(:,1)),diff(poincareSection(i).endPosition(:,2)));
+    poincareSection(i).orbitMaxLength = 2*(2*pi*rOrbit);
+end
+
+[shearline.etaPos,shearline.etaNeg] = lagrangian_shear(flow.cgEigenvector,flow.cgEigenvalue);
+
 % Compute closed shearlines
-% Define Poincare sections
-% Place first point in center of elliptic region and second point outside
-% elliptic region
-poincareSection{1}.endPosition = [.5,.6;.35,.5];
-poincareSection{1}.numPoints = 80;
-% set maximum integration length to twice the expected circumference
-rOrbit = hypot(diff(poincareSection{1}.endPosition(:,1)),diff(poincareSection{1}.endPosition(:,2)));
-poincareSection{1}.integrationLength = [0,2*(2*pi*rOrbit)];
+closedOrbits = poincare_closed_orbit_multi(flow,shearline,poincareSection,'showGraph',true);
 
-% Plot Poincare section
-hPoincareSection = arrayfun(@(idx)plot(hAxes,poincareSection{idx}.endPosition(:,1),poincareSection{idx}.endPosition(:,2)),numel(poincareSection));
-set(hPoincareSection,'color',shearLcsColor)
-set(hPoincareSection,'linestyle','--')
+% Plot elliptic LCS
+% η₊ outermost closed orbit
+hClosedOrbitsEtaPos = arrayfun(@(i)plot(hAxes,closedOrbits{i}{1}{end}(:,1),closedOrbits{i}{1}{end}(:,2)),1:size(closedOrbits,2));
+set(hClosedOrbitsEtaPos,'color',shearLcsColor)
+set(hClosedOrbitsEtaPos,'linewidth',2)
+% η₋ outermost closed orbit
+hClosedOrbitsEtaNeg = arrayfun(@(i)plot(hAxes,closedOrbits{i}{2}{end}(:,1),closedOrbits{i}{2}{end}(:,2)),1:size(closedOrbits,2));
+set(hClosedOrbitsEtaNeg,'color',shearLcsColor)
+set(hClosedOrbitsEtaNeg,'linewidth',2)
+
+% Plot all closed orbits of Poincare sections
+for j = 1:nPoincareSection
+    % η₊
+    hOrbitsPos = cellfun(@(position)plot(hAxes,position(:,1),position(:,2)),closedOrbits{j}{1});
+    set(hOrbitsPos,'color',shearLcsColor)
+    % η₋
+    hOrbitsNeg = cellfun(@(position)plot(hAxes,position(:,1),position(:,2)),closedOrbits{j}{2});
+    set(hOrbitsNeg,'color',shearLcsColor)
+end
 drawnow
-
-% Find closed orbits with Poincare section return map
-[etaPos,etaNeg] = lagrangian_shear(doubleGyre.flow.cgEigenvector,doubleGyre.flow.cgEigenvalue);
-showGraph = true;
-odeSolverOptions = odeset('relTol',1e-3);
-nBisection = 2;
-dThresh = 1e-2;
-closedOrbitPos = poincare_closed_orbit(doubleGyre.flow,etaPos,poincareSection{1},odeSolverOptions,nBisection,dThresh,showGraph);
-
-% Plot closed orbits
-hClosedOrbits = cellfun(@(position)plot(hAxes,position(:,1),position(:,2)),closedOrbitPos);
-set(hClosedOrbits,'color',shearLcsColor)
-hOuterClosedOrbit = plot(hAxes,closedOrbitPos{end}(:,1),closedOrbitPos{end}(:,2));
-set(hOuterClosedOrbit,'color',shearLcsColor)
-set(hOuterClosedOrbit,'LineWidth',2)
-
-% Repeat for second Poincare section, with etaNeg vector
-poincareSection{1}.endPosition = [1.5,.4;1.7,.5];
-rOrbit = hypot(diff(poincareSection{1}.endPosition(:,1)),diff(poincareSection{1}.endPosition(:,2)));
-poincareSection{1}.integrationLength = [0,2*(2*pi*rOrbit)];
-hPoincareSection = arrayfun(@(idx)plot(hAxes,poincareSection{idx}.endPosition(:,1),poincareSection{idx}.endPosition(:,2)),numel(poincareSection));
-set(hPoincareSection,'color',shearLcsColor)
-set(hPoincareSection,'linestyle','--')
-drawnow
-
-closedOrbitNeg = poincare_closed_orbit(doubleGyre.flow,etaNeg,poincareSection{1},odeSolverOptions,nBisection,dThresh,showGraph);
-
-hClosedOrbits = cellfun(@(position)plot(hAxes,position(:,1),position(:,2)),closedOrbitNeg);
-set(hClosedOrbits,'color',shearLcsColor)
-hOuterClosedOrbit = plot(hAxes,closedOrbitNeg{end}(:,1),closedOrbitNeg{end}(:,2));
-set(hOuterClosedOrbit,'color',shearLcsColor)
-set(hOuterClosedOrbit,'LineWidth',2)
 
 % Compute strainlines
-[strainlinePosition,strainlineInitialPosition] = seed_curves_from_lambda_max(localMaxDistance,doubleGyre.strainline.maxLength,cgEigenvalue(:,:,2),cgEigenvector(:,:,1:2),doubleGyre.flow.domain,doubleGyre.flow.periodicBc);
-strainlinePosition = remove_strain_in_shear(strainlinePosition,closedOrbitPos{end});
-strainlinePosition = remove_strain_in_shear(strainlinePosition,closedOrbitNeg{end});
+[strainlinePosition,strainlineInitialPosition] = seed_curves_from_lambda_max(localMaxDistance,strainlineMaxLength,cgEigenvalue(:,:,2),cgEigenvector(:,:,1:2),flow.domain);
+
+for i = 1:nPoincareSection
+    % Remove strainlines inside of ellitpic regions
+    strainlinePosition = remove_strain_in_shear(strainlinePosition,closedOrbits{i}{1}{end});
+    strainlinePosition = remove_strain_in_shear(strainlinePosition,closedOrbits{i}{2}{end});
+    % Remove initial positions inside elliptic regions
+    idx = inpolygon(strainlineInitialPosition(1,:),strainlineInitialPosition(2,:),closedOrbits{i}{1}{end}(:,1),closedOrbits{i}{1}{end}(:,2));
+    strainlineInitialPosition = strainlineInitialPosition(:,~idx);
+    idx = inpolygon(strainlineInitialPosition(1,:),strainlineInitialPosition(2,:),closedOrbits{i}{2}{end}(:,1),closedOrbits{i}{2}{end}(:,2));
+    strainlineInitialPosition = strainlineInitialPosition(:,~idx);
+end
 
 % Plot strainlines
 hStrainline = cellfun(@(position)plot(hAxes,position(:,1),position(:,2)),strainlinePosition);
@@ -101,71 +112,81 @@ drawnow
 
 %% Backward-time LCS analysis
 % Compute Cauchy-Green strain eigenvalues and eigenvectors
-doubleGyreBackward = doubleGyre;
-doubleGyreBackward.flow = set_flow_timespan([timespan,0],doubleGyre.flow);
-[doubleGyreBackward.flow.cgEigenvalue,doubleGyreBackward.flow.cgEigenvector] = eig_cgStrain(doubleGyreBackward.flow);
-cgEigenvalue = reshape(doubleGyreBackward.flow.cgEigenvalue,[fliplr(doubleGyreBackward.flow.resolution),2]);
-cgEigenvector = reshape(doubleGyreBackward.flow.cgEigenvector,[fliplr(doubleGyreBackward.flow.resolution),4]);
+flow.timespan = [timespan,0];
+[flow.cgEigenvalue,flow.cgEigenvector] = eig_cgStrain(flow);
+cgEigenvalue = reshape(flow.cgEigenvalue,[fliplr(flow.resolution),2]);
+cgEigenvector = reshape(flow.cgEigenvector,[fliplr(flow.resolution),4]);
 
 % Plot backward time finite-time Lyapunov exponent
-ftleBackward = compute_ftle(cgEigenvalue(:,:,2),diff(doubleGyreBackward.flow.timespan));
-hAxes = setup_figure(doubleGyreBackward.flow.domain);
+ftle = compute_ftle(cgEigenvalue(:,:,2),diff(flow.timespan));
+hAxes = setup_figure(flow.domain);
 title(hAxes,'Backward-time LCS')
-plot_ftle(hAxes,doubleGyreBackward.flow,ftleBackward);
+plot_ftle(hAxes,flow,ftle);
 colormap(hAxes,gray)
 drawnow
 
-% Compute closed shearlines
 % Define Poincare sections
-% Place first point in center of elliptic region and second point outside
-% elliptic region
-poincareSection{1}.endPosition = [.5,.6;.3,.5];
-poincareSection{1}.numPoints = 80;
-% set maximum integration length to twice the expected circumference
-rOrbit = hypot(diff(poincareSection{1}.endPosition(:,1)),diff(poincareSection{1}.endPosition(:,2)));
-poincareSection{1}.integrationLength = [0,2*(2*pi*rOrbit)];
+poincareSection(1).endPosition = [.5,.6;.35,.5];
+poincareSection(2).endPosition = [1.5,.4;1.7,.5];
 
-% Plot Poincare section
-hPoincareSection = arrayfun(@(idx)plot(hAxes,poincareSection{idx}.endPosition(:,1),poincareSection{idx}.endPosition(:,2)),numel(poincareSection));
+% Plot Poincare sections
+hPoincareSection = arrayfun(@(input)plot(hAxes,input.endPosition(:,1),input.endPosition(:,2)),poincareSection);
 set(hPoincareSection,'color',shearLcsColor)
-set(hPoincareSection,'linestyle','--')
+set(hPoincareSection,'LineStyle','--')
+set(hPoincareSection,'marker','o')
+set(hPoincareSection,'MarkerFaceColor',shearLcsColor)
+set(hPoincareSection,'MarkerEdgeColor','w')
 drawnow
 
-% Find closed orbits with Poincare section return map
-[etaPos,etaNeg] = lagrangian_shear(doubleGyreBackward.flow.cgEigenvector,doubleGyreBackward.flow.cgEigenvalue);
-showGraph = true;
-odeSolverOptions = odeset('relTol',1e-3);
-nBisection = 2;
-dThresh = 1e-2;
-closedOrbitPos = poincare_closed_orbit(doubleGyreBackward.flow,etaNeg,poincareSection{1},odeSolverOptions,nBisection,dThresh,showGraph);
+% Number of orbit seed points along each Poincare section
+[poincareSection.numPoints] = deal(80);
 
-hClosedOrbits = cellfun(@(position)plot(hAxes,position(:,1),position(:,2)),closedOrbitPos);
-set(hClosedOrbits,'color',shearLcsColor)
-hOuterClosedOrbit = plot(hAxes,closedOrbitPos{end}(:,1),closedOrbitPos{end}(:,2));
-set(hOuterClosedOrbit,'color',shearLcsColor)
-set(hOuterClosedOrbit,'LineWidth',2)
+% Set maximum orbit length to twice the expected circumference
+nPoincareSection = numel(poincareSection);
+for i = 1:nPoincareSection
+    rOrbit = hypot(diff(poincareSection(i).endPosition(:,1)),diff(poincareSection(i).endPosition(:,2)));
+    poincareSection(i).orbitMaxLength = 2*(2*pi*rOrbit);
+end
 
-% Repeat for second Poincare section, with etaPos vector
-poincareSection{1}.endPosition = [1.5,.4;1.7,.5];
-rOrbit = hypot(diff(poincareSection{1}.endPosition(:,1)),diff(poincareSection{1}.endPosition(:,2)));
-poincareSection{1}.integrationLength = [0,2*(2*pi*rOrbit)];
-hPoincareSection = arrayfun(@(idx)plot(hAxes,poincareSection{idx}.endPosition(:,1),poincareSection{idx}.endPosition(:,2)),numel(poincareSection));
-set(hPoincareSection,'color',shearLcsColor)
-set(hPoincareSection,'linestyle','--')
+[shearline.etaPos,shearline.etaNeg] = lagrangian_shear(flow.cgEigenvector,flow.cgEigenvalue);
+
+% Compute closed shearlines
+closedOrbits = poincare_closed_orbit_multi(flow,shearline,poincareSection,'showGraph',true);
+
+% Plot elliptic LCS
+% η₊ outermost closed orbit
+hClosedOrbitsEtaPos = arrayfun(@(i)plot(hAxes,closedOrbits{i}{1}{end}(:,1),closedOrbits{i}{1}{end}(:,2)),1:size(closedOrbits,2));
+set(hClosedOrbitsEtaPos,'color',shearLcsColor)
+set(hClosedOrbitsEtaPos,'linewidth',2)
+% η₋ outermost closed orbit
+hClosedOrbitsEtaNeg = arrayfun(@(i)plot(hAxes,closedOrbits{i}{2}{end}(:,1),closedOrbits{i}{2}{end}(:,2)),1:size(closedOrbits,2));
+set(hClosedOrbitsEtaNeg,'color',shearLcsColor)
+set(hClosedOrbitsEtaNeg,'linewidth',2)
+
+% Plot all closed orbits of Poincare sections
+for j = 1:nPoincareSection
+    % η₊
+    hOrbitsPos = cellfun(@(position)plot(hAxes,position(:,1),position(:,2)),closedOrbits{j}{1});
+    set(hOrbitsPos,'color',shearLcsColor)
+    % η₋
+    hOrbitsNeg = cellfun(@(position)plot(hAxes,position(:,1),position(:,2)),closedOrbits{j}{2});
+    set(hOrbitsNeg,'color',shearLcsColor)
+end
 drawnow
-
-closedOrbitNeg = poincare_closed_orbit(doubleGyre.flow,etaPos,poincareSection{1},odeSolverOptions,nBisection,dThresh,showGraph);
-
-hClosedOrbits = cellfun(@(position)plot(hAxes,position(:,1),position(:,2)),closedOrbitNeg);
-set(hClosedOrbits,'color',shearLcsColor)
-hOuterClosedOrbit = plot(hAxes,closedOrbitNeg{end}(:,1),closedOrbitNeg{end}(:,2));
-set(hOuterClosedOrbit,'color',shearLcsColor)
-set(hOuterClosedOrbit,'LineWidth',2)
 
 % Compute strainlines
-[strainlinePosition,strainlineInitialPosition] = seed_curves_from_lambda_max(localMaxDistance,doubleGyreBackward.strainline.maxLength,cgEigenvalue(:,:,2),cgEigenvector(:,:,1:2),doubleGyreBackward.flow.domain,doubleGyreBackward.flow.periodicBc);
-strainlinePosition = remove_strain_in_shear(strainlinePosition,closedOrbitPos{end});
-strainlinePosition = remove_strain_in_shear(strainlinePosition,closedOrbitNeg{end});
+[strainlinePosition,strainlineInitialPosition] = seed_curves_from_lambda_max(localMaxDistance,strainlineMaxLength,cgEigenvalue(:,:,2),cgEigenvector(:,:,1:2),flow.domain);
+
+for i = 1:nPoincareSection
+    % Remove strainlines inside of ellitpic regions
+    strainlinePosition = remove_strain_in_shear(strainlinePosition,closedOrbits{i}{1}{end});
+    strainlinePosition = remove_strain_in_shear(strainlinePosition,closedOrbits{i}{2}{end});
+    % Remove initial positions inside elliptic regions
+    idx = inpolygon(strainlineInitialPosition(1,:),strainlineInitialPosition(2,:),closedOrbits{i}{1}{end}(:,1),closedOrbits{i}{1}{end}(:,2));
+    strainlineInitialPosition = strainlineInitialPosition(:,~idx);
+    idx = inpolygon(strainlineInitialPosition(1,:),strainlineInitialPosition(2,:),closedOrbits{i}{2}{end}(:,1),closedOrbits{i}{2}{end}(:,2));
+    strainlineInitialPosition = strainlineInitialPosition(:,~idx);
+end
 
 % Plot strainlines
 hStrainline = cellfun(@(position)plot(hAxes,position(:,1),position(:,2)),strainlinePosition);
