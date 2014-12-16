765475df612004103fd1138b213f13ac148aa4f6
Kristjan Onu <jeixav@gmail.com>
2014-12-16 16:26:08 -0500

    Update Bickley jet demo parameters

--- demo/bickley_jet/elliptic_hyperbolic_lcs.m
+++ demo/bickley_jet/elliptic_hyperbolic_lcs.m
@@ -29,25 +29,25 @@ poincareSection(2).endPosition = [6.5e6,-1.4e6;5e6,-3e6];
 poincareSection(3).endPosition = [1e7,1.5e6;8e6,2.6e6];
 poincareSection(4).endPosition = [1.35e7,-1.4e6;1.5e7,-.5e6];
 poincareSection(5).endPosition = [1.65e7, 1.5e6;1.5e7, 2.6e6];
-[poincareSection.numPoints] = deal(80);
+[poincareSection.numPoints] = deal(20);
 nPoincareSection = numel(poincareSection);
 for i = 1:nPoincareSection
     rOrbit = hypot(diff(poincareSection(i).endPosition(:,1)),diff(poincareSection(i).endPosition(:,2)));
     poincareSection(i).orbitMaxLength = 2*(2*pi*rOrbit);
 end
 lambda = .9:.01:1.1;
-lambdaLineOdeSolverOptions = odeset('relTol',1e-6);
+lambdaLineOdeSolverOptions = odeset('absTol',10);
 forceEtaComplexNaN = true;
 
 % Shrink lines
 shrinkLineMaxLength = 1e8;
 shrinkLineLocalMaxDistance = 8*deltaX;
-shrinkLineOdeSolverOptions = odeset('relTol',1e-4);
+shrinkLineOdeSolverOptions = odeset('absTol',1);
 
 % Stretch lines
 stretchLineMaxLength = 1e8;
-stretchLineLocalMaxDistance = 4*deltaX;
-stretchLineOdeSolverOptions = odeset('relTol',1e-4);
+stretchLineLocalMaxDistance = 8*deltaX;
+stretchLineOdeSolverOptions = odeset('absTol',10);
 
 % Graphic properties
 repellingColor = 'r';
@@ -61,7 +61,11 @@ title(hAxes,'Repelling and elliptic LCSs')
 [cgEigenvector,cgEigenvalue] = eig_cgStrain(lDerivative,domain,resolution,timespan,'incompressible',incompressible);
 
 %% Elliptic LCSs
-[closedLambdaLinePos,closedLambdaLineNeg] = poincare_closed_orbit_range(domain,resolution,cgEigenvector,cgEigenvalue,lambda,poincareSection,'forceEtaComplexNaN',forceEtaComplexNaN,'odeSolverOptions',lambdaLineOdeSolverOptions,'periodicBc',periodicBc);
+[closedLambdaLinePos,closedLambdaLineNeg] = poincare_closed_orbit_range(...
+    domain,resolution,cgEigenvector,cgEigenvalue,lambda,poincareSection,...
+    'forceEtaComplexNaN',forceEtaComplexNaN,...
+    'odeSolverOptions',lambdaLineOdeSolverOptions,...
+    'periodicBc',periodicBc);
 
 ellipticLcs = elliptic_lcs(closedLambdaLinePos);
 ellipticLcs = [ellipticLcs,elliptic_lcs(closedLambdaLineNeg)];
@@ -73,7 +77,11 @@ set(hEllipticLcs,'linewidth',2)
 drawnow
 
 %% Hyperbolic repelling LCSs
-shrinkLine = seed_curves_from_lambda_max(shrinkLineLocalMaxDistance,shrinkLineMaxLength,cgEigenvalue(:,2),cgEigenvector(:,1:2),domain,resolution,'odeSolverOptions',shrinkLineOdeSolverOptions,'periodicBc',periodicBc);
+shrinkLine = seed_curves_from_lambda_max(shrinkLineLocalMaxDistance,...
+    shrinkLineMaxLength,cgEigenvalue(:,2),cgEigenvector(:,1:2),domain,...
+    resolution,...
+    'odeSolverOptions',shrinkLineOdeSolverOptions,...
+    'periodicBc',periodicBc);
 
 % Remove shrink lines inside elliptic LCSs
 for i = 1:nPoincareSection
@@ -99,7 +107,11 @@ drawnow
 % FIXME Part of calculations in seed_curves_from_lambda_max are
 % unsuitable/unecessary for stretchlines do not follow ridges of λ₁
 % minima
-stretchLine = seed_curves_from_lambda_max(stretchLineLocalMaxDistance,stretchLineMaxLength,-cgEigenvalue(:,1),cgEigenvector(:,3:4),domain,resolution,'odeSolverOptions',stretchLineOdeSolverOptions,'periodicBc',periodicBc);
+stretchLine = seed_curves_from_lambda_max(stretchLineLocalMaxDistance,...
+    stretchLineMaxLength,-cgEigenvalue(:,1),cgEigenvector(:,3:4),domain,...
+    resolution,...
+    'odeSolverOptions',stretchLineOdeSolverOptions,...
+    'periodicBc',periodicBc);
 
 % Remove stretch lines inside elliptic LCSs
 for i = 1:nPoincareSection
--- demo/bickley_jet/elliptic_hyperbolic_lcs_details.m
+++ demo/bickley_jet/elliptic_hyperbolic_lcs_details.m
@@ -29,25 +29,25 @@ poincareSection(2).endPosition = [6.5e6,-1.4e6;5e6,-3e6];
 poincareSection(3).endPosition = [1e7,1.5e6;8e6,2.6e6];
 poincareSection(4).endPosition = [1.35e7,-1.4e6;1.5e7,-.5e6];
 poincareSection(5).endPosition = [1.65e7, 1.5e6;1.5e7, 2.6e6];
-[poincareSection.numPoints] = deal(80);
+[poincareSection.numPoints] = deal(20);
 nPoincareSection = numel(poincareSection);
 for i = 1:nPoincareSection
     rOrbit = hypot(diff(poincareSection(i).endPosition(:,1)),diff(poincareSection(i).endPosition(:,2)));
     poincareSection(i).orbitMaxLength = 2*(2*pi*rOrbit);
 end
 lambda = .9:.01:1.1;
-lambdaLineOdeSolverOptions = odeset('relTol',1e-6);
+lambdaLineOdeSolverOptions = odeset('absTol',10);
 forceEtaComplexNaN = true;
 
 % Shrink lines
 shrinkLineMaxLength = 1e8;
 shrinkLineLocalMaxDistance = 8*deltaX;
-shrinkLineOdeSolverOptions = odeset('relTol',1e-4);
+shrinkLineOdeSolverOptions = odeset('absTol',1);
 
 % Stretch lines
 stretchLineMaxLength = 1e8;
-stretchLineLocalMaxDistance = 4*deltaX;
-stretchLineOdeSolverOptions = odeset('relTol',1e-4);
+stretchLineLocalMaxDistance = 8*deltaX;
+stretchLineOdeSolverOptions = odeset('absTol',10);
 
 % Graphic properties
 repellingColor = 'r';
@@ -79,7 +79,11 @@ set(hPoincareSection,'MarkerFaceColor',ellipticColor)
 set(hPoincareSection,'MarkerEdgeColor','w')
 drawnow
 
-[closedLambdaLinePos,closedLambdaLineNeg] = poincare_closed_orbit_range(domain,resolution,cgEigenvector,cgEigenvalue,lambda,poincareSection,'forceEtaComplexNaN',forceEtaComplexNaN,'odeSolverOptions',lambdaLineOdeSolverOptions,'periodicBc',periodicBc);
+[closedLambdaLinePos,closedLambdaLineNeg] = poincare_closed_orbit_range(...
+    domain,resolution,cgEigenvector,cgEigenvalue,lambda,poincareSection,...
+    'forceEtaComplexNaN',forceEtaComplexNaN,...
+    'odeSolverOptions',lambdaLineOdeSolverOptions,...
+    'periodicBc',periodicBc);
 
 ellipticLcs = elliptic_lcs(closedLambdaLinePos);
 ellipticLcs = [ellipticLcs,elliptic_lcs(closedLambdaLineNeg)];
@@ -97,7 +101,11 @@ set(hClosedLambdaLine,'color',ellipticColor)
 drawnow
 
 %% Hyperbolic repelling LCSs
-[shrinkLine,shrinkLineInitialPosition] = seed_curves_from_lambda_max(shrinkLineLocalMaxDistance,shrinkLineMaxLength,cgEigenvalue(:,2),cgEigenvector(:,1:2),domain,resolution,'odeSolverOptions',shrinkLineOdeSolverOptions);
+[shrinkLine,shrinkLineInitialPosition] = seed_curves_from_lambda_max(...
+    shrinkLineLocalMaxDistance,shrinkLineMaxLength,cgEigenvalue(:,2),...
+    cgEigenvector(:,1:2),domain,resolution,...
+    'odeSolverOptions',shrinkLineOdeSolverOptions,...
+    'periodicBc',periodicBc);
 
 % Remove shrink lines inside elliptic LCSs
 for i = 1:nPoincareSection
@@ -139,7 +147,11 @@ drawnow
 % FIXME Part of calculations in seed_curves_from_lambda_max are
 % unsuitable/unecessary for stretchlines do not follow ridges of λ₁
 % minima
-[stretchLine,stretchLineInitialPosition] = seed_curves_from_lambda_max(stretchLineLocalMaxDistance,stretchLineMaxLength,-cgEigenvalue(:,1),cgEigenvector(:,3:4),domain,resolution,'odeSolverOptions',stretchLineOdeSolverOptions);
+[stretchLine,stretchLineInitialPosition] = seed_curves_from_lambda_max(...
+    stretchLineLocalMaxDistance,stretchLineMaxLength,-cgEigenvalue(:,1),...
+    cgEigenvector(:,3:4),domain,resolution,...
+    'odeSolverOptions',stretchLineOdeSolverOptions,...
+    'periodicBc',periodicBc);
 
 % Remove stretch lines inside elliptic LCSs
 for i = 1:nPoincareSection
